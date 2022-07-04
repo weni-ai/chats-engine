00b4d0e55c23417a4a9cdaf506a8730bf0f2e675
helllllllder <heldergsds@gmail.com>
2022-07-04 15:13:14 -0300

    remove testing templates

--- chats/apps/rooms/urls.py
+++
@@ -1,9 +0,0 @@
-# chat/urls.py
-from django.urls import path
-
-from chats.apps.rooms import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("<str:room_name>/", views.room, name="room"),
-]
--- chats/urls.py
+++ chats/urls.py
@@ -37,6 +37,5 @@ schema_view = get_schema_view(
 urlpatterns = [
     path("", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
     path("admin/", admin.site.urls),
-    path("room/", include("chats.apps.rooms.urls")),
     path("v1/", include("chats.apps.api.v1.urls")),
 ]
