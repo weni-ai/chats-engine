60bdba15eaea7a983be7d27f05a00f78087cbc2a
helllllllder <heldergsds@gmail.com>
2022-08-07 20:42:02 -0300

    feat: add filters, serializers and modify contact viewset

--- chats/apps/api/v1/contacts/viewsets.py
+++ chats/apps/api/v1/contacts/viewsets.py
@@ -1,7 +1,13 @@
+from django.db.models import Q
+
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
 
 from chats.apps.api.v1.contacts.serializers import ContactSerializer
 from chats.apps.contacts.models import Contact
+from chats.apps.api.v1.contacts.filters import ContactFilter
 
 
 class ContactViewset(
@@ -9,7 +15,25 @@ class ContactViewset(
     viewsets.mixins.ListModelMixin,
     viewsets.mixins.RetrieveModelMixin,
 ):
+
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
-    # TODO: Include `chats.apps.api.v1.permissions.SectorAnyPermission` in permission_classes list
-    permission_classes = []
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = ContactFilter
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        qs = self.queryset
+        user = self.request.user
+        is_manager = Q(rooms__queue__sector__authorizations__user=user)
+        is_admin = Q(
+            Q(rooms__queue__sector__project__authorizations__user=user)
+            & Q(rooms__queue__sector__project__authorizations__role=1)
+        )
+        is_user_assigned_to_room = Q(rooms__user=user)
+
+        check_admin_manager_agent_role_filter = (
+            is_manager | is_admin | is_user_assigned_to_room
+        )
+        user_role_related_contacts = qs.filter(check_admin_manager_agent_role_filter)
+        return user_role_related_contacts
--- chats/apps/contacts/models.py
+++ chats/apps/contacts/models.py
@@ -7,6 +7,12 @@ from chats.core.models import BaseModel
 class Contact(BaseModel):
     name = models.CharField(_("first name"), max_length=30, blank=True)
     email = models.EmailField(_("email"), unique=True, help_text=_("User email"))
+    status = models.CharField(_("status"), max_length=30, blank=True)
+    custom_fields = models.JSONField(
+        _("custom fields"),
+        blank=True,
+        null=True,
+    )
 
     class Meta:
         verbose_name = _("Contact")
@@ -16,7 +22,22 @@ class Contact(BaseModel):
         return self.email
 
     @property
+    def last_agent_name(self):
+        try:
+            return self.rooms.last().user.name
+        except AttributeError:
+            return ""
+
+    @property
     def serialized_ws_data(self):
         from chats.apps.api.v1.contacts.serializers import ContactWSSerializer
 
         return ContactWSSerializer(self).data
+
+    @property
+    def last_room(self):
+        return self.rooms.filter(is_active=True).last()
+
+    @property
+    def tags(self):
+        return self.last_room.tags
