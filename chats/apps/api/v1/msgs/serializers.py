b2fda9fd16e1b404df3f403f29174c530fee576c
helllllllder <heldergsds@gmail.com>
2022-08-25 18:22:00 -0300

    feat: add user and contact objects into messageserializer

--- chats/apps/api/v1/msgs/serializers.py
+++ chats/apps/api/v1/msgs/serializers.py
@@ -4,18 +4,32 @@ from django.conf import settings
 from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 
+from chats.apps.api.v1.accounts.serializers import UserSerializer
+from chats.apps.api.v1.contacts.serializers import ContactSerializer
 from chats.apps.msgs.models import Message as ChatMessage
 from chats.apps.msgs.models import MessageMedia
+from chats.apps.accounts.models import User
 
 
 class MessageSerializer(serializers.ModelSerializer):
     media = serializers.FileField(required=False)
+    contact = ContactSerializer(many=False, required=False, read_only=True)
+    user = UserSerializer(many=False, required=False, read_only=True)
+    user_email = serializers.SlugRelatedField(
+        queryset=User.objects.all(),
+        required=False,
+        source="user",
+        slug_field="email",
+        write_only=True,
+        allow_null=True,
+    )
 
     class Meta:
         model = ChatMessage
         fields = [
-            "room",
+            "user_email",
             "user",
+            "room",
             "contact",
             "text",
             "seen",
@@ -23,6 +37,7 @@ class MessageSerializer(serializers.ModelSerializer):
             "created_on",
         ]
         read_only_fields = [
+            "user",
             "created_at",
             "contact",
         ]
--- chats/utils/websockets.py
+++ chats/utils/websockets.py
@@ -1,3 +1,7 @@
+import json
+
+from django.core.serializers.json import DjangoJSONEncoder
+
 from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
 
@@ -6,12 +10,15 @@ def send_channels_group(group_name: str, type: str, content: str, action: str):
     """
     helper function that sends data to channels groups
     """
+    import pdb
+
+    pdb.set_trace()
     channel_layer = get_channel_layer()
     async_to_sync(channel_layer.group_send)(
         group_name,
         {
             "type": type,
             "action": action,
-            "content": content,
+            "content": json.dumps(content, cls=DjangoJSONEncoder),
         },
     )
