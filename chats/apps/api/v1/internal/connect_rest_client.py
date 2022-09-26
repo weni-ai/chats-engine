7a22ce480ea69b4348197d30ddc85bc621e6b1fc
helllllllder <heldergsds@gmail.com>
2022-09-26 18:43:52 -0300

    feat: Add queue flows callback calls

--- chats/apps/api/v1/internal/connect_rest_client.py
+++ chats/apps/api/v1/internal/connect_rest_client.py
@@ -12,21 +12,25 @@ class ConnectRESTClient:
         }
 
     def get_auth_token(self) -> str:
-        request = requests.post(
-            url=settings.OIDC_OP_TOKEN_ENDPOINT,
-            data={
-                "client_id": settings.OIDC_RP_CLIENT_ID,
-                "client_secret": settings.OIDC_RP_CLIENT_SECRET,
-                "grant_type": "client_credentials",
-            },
-        )
-        token = request.json().get("access_token")
+        if settings.OIDC_ENABLED:
+
+            request = requests.post(
+                url=settings.OIDC_OP_TOKEN_ENDPOINT,
+                data={
+                    "client_id": settings.OIDC_RP_CLIENT_ID,
+                    "client_secret": settings.OIDC_RP_CLIENT_SECRET,
+                    "grant_type": "client_credentials",
+                },
+            )
+            token = request.json().get("access_token")
+        else:
+            token = ""
         return f"Bearer {token}"
 
     def create_ticketer(self, **kwargs):
-        request = requests.post(
+        response = requests.post(
             url=f"{self.base_url}/v1/organization/project/create_ticketer/",
             headers=self.headers,
             params={**kwargs, "ticketer_type": "chats"},
         )
-        return request.json()
+        return response
