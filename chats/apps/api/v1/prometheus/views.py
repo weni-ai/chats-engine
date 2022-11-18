290892c61c5a91c23fe92026f89d48b3395da871
AlanJaeger <aland295@gmail.com>
2022-11-18 17:48:07 -0300

    feature:prometheus auth token for authentication in metrics view

--- chats/apps/api/v1/prometheus/views.py
+++ chats/apps/api/v1/prometheus/views.py
@@ -2,6 +2,17 @@ from prometheus_client import CONTENT_TYPE_LATEST, generate_latest
 
 from django.http import HttpResponse
 
+from django.conf import settings
 
 def metrics_view(request):
-    return HttpResponse(generate_latest(), content_type=CONTENT_TYPE_LATEST)
+    try:
+        authentication_token = settings.PROMETHEUS_AUTH_TOKEN
+        header = request.META.get('HTTP_AUTHORIZATION', None)
+        token = header.split()[1]
+        if authentication_token == token:  
+            return HttpResponse(generate_latest(), content_type=CONTENT_TYPE_LATEST)
+        else:
+            return HttpResponse("Detail: You dont have permission to access metrics", status=401)
+    except AttributeError:
+        return HttpResponse("Detail: You dont have permission to access metrics", status=401)
+    
