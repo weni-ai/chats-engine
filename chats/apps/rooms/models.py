7032b941237b27b9f511130fb27dd6922dea2b15
helllllllder <heldergsds@gmail.com>
2022-12-19 17:06:02 -0300

    feat: update the is_24h_valid property of Room, to only validate the day if the urn is from a whatsapp

--- chats/apps/rooms/models.py
+++ chats/apps/rooms/models.py
@@ -95,6 +95,8 @@ class Room(BaseModel):
     @property
     def is_24h_valid(self) -> bool:
         """Validates is the last contact message was sent more than a day ago"""
+        if not self.urn.startswith("whatsapp"):
+            return True
         day_validation = self.messages.filter(
             created_on__gte=timezone.now() - timedelta(days=1),
             contact=self.contact,
