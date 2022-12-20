25459ddf502c434582b5b547aabdfe49d22196fc
alandavl <aland295@gmail.com>
2022-12-20 10:23:34 -0300

    Feature/increasing date accuracy for grafana dash (#84)
    * fix/correct choices to user status
    
    * feature: increasing date accuracy for prometheus metrics and grafana dashboard

--- chats/apps/rooms/signals.py
+++ chats/apps/rooms/signals.py
@@ -1,24 +1,18 @@
-from chats.apps.rooms.models import Room
 from django.dispatch import receiver
 from django.db.models.signals import post_save, post_delete
-from datetime import datetime
-from dateutil.relativedelta import *
+from chats.apps.rooms.models import Room
+from dateutil.relativedelta import relativedelta
+from django.utils import timezone
 
 from chats.apps.api.v1.prometheus.metrics import(
-    total_rooms, 
-    opened_rooms, 
-    closed_rooms, 
-    total_rooms_last_month,
-    total_rooms_last_3_months,
-    total_rooms_last_6_months,
-    total_rooms_last_year
+    chats_total_rooms, 
+    chats_opened_rooms, 
+    chats_closed_rooms, 
+    chats_total_rooms_last_month,
+    chats_total_rooms_last_3_months,
+    chats_total_rooms_last_6_months,
+    chats_total_rooms_last_year
 )
-
-now = datetime.now()
-last_month = now + relativedelta(months=-1)
-last_3_months = now + relativedelta(months=-3)
-last_6_months = now + relativedelta(months=-6)
-last_year = now + relativedelta(years=-1)
 
 @receiver([post_save, post_delete], sender=Room)
 def rooms_metrics_sender(sender, instance, **kwargs):
@@ -33,28 +27,40 @@ class Metrics():
 
     def metric_total_rooms(self):
         rooms_count = Room.objects.all().count()
-        total_rooms.labels("total_rooms").set(rooms_count)
+        chats_total_rooms.labels("total_rooms").set(rooms_count)
 
     def metric_opened_rooms(self):
         open_rooms_count = Room.objects.filter(ended_at__isnull = True).count()
-        opened_rooms.labels("opened_rooms").set(open_rooms_count)
+        chats_opened_rooms.labels("opened_rooms").set(open_rooms_count)
 
     def metric_closed_rooms(self):
         closed_rooms_count = Room.objects.filter(ended_at__isnull = False).count()
-        closed_rooms.labels("closed_rooms").set(closed_rooms_count)
+        chats_closed_rooms.labels("closed_rooms").set(closed_rooms_count)
 
     def metric_total_rooms_created_last_month(self):
-        last_month_rooms_count = Room.objects.filter(created_on__range=[last_month,now]).count()
-        total_rooms_last_month.labels("total_rooms_last_month").set(last_month_rooms_count)
+        now = timezone.now()
+        last_month = now + relativedelta(months=-1)
+
+        last_month_rooms_count = Room.objects.filter(created_on__gte=last_month).count()
+        chats_total_rooms_last_month.labels("total_rooms_last_month").set(last_month_rooms_count)
 
     def metric_total_rooms_created_last_3_months(self):
-        last_3_months_rooms_count = Room.objects.filter(created_on__range=[last_3_months,now]).count()
-        total_rooms_last_3_months.labels("total_rooms_last_3_months").set(last_3_months_rooms_count)
+        now = timezone.now()
+        last_3_months = now + relativedelta(months=-3)
 
-    def metric_total_rooms_created_last_3_months(self):
-        last_6_months_rooms_count = Room.objects.filter(created_on__range=[last_6_months,now]).count()
-        total_rooms_last_6_months.labels("total_rooms_last_6_months").set(last_6_months_rooms_count)
+        last_3_months_rooms_count = Room.objects.filter(created_on__gte=last_3_months).count()
+        chats_total_rooms_last_3_months.labels("total_rooms_last_3_months").set(last_3_months_rooms_count)
+
+    def metric_total_rooms_created_last_6_months(self):
+        now = timezone.now()
+        last_6_months = now + relativedelta(months=-6)
+
+        last_6_months_rooms_count = Room.objects.filter(created_on__gte=last_6_months).count()
+        chats_total_rooms_last_6_months.labels("total_rooms_last_6_months").set(last_6_months_rooms_count)
 
     def metric_total_rooms_created_last_year(self):
-        last_year_rooms_count = Room.objects.filter(created_on__range=[last_year,now]).count()
-        total_rooms_last_year.labels("total_rooms_last_year").set(last_year_rooms_count)
+        now = timezone.now()
+        last_year = now + relativedelta(years=-1)
+        
+        last_year_rooms_count = Room.objects.filter(created_on__gte=last_year).count()
+        chats_total_rooms_last_year.labels("total_rooms_last_year").set(last_year_rooms_count)
