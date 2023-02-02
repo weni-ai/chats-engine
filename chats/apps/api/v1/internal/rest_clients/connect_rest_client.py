61428f4b8012ee631fb202bf4f2146fc5316a7e0
helllllllder <heldergsds@gmail.com>
2023-02-02 10:38:52 -0300

    feat: use connect V2 endpoints

--- chats/apps/api/v1/internal/rest_clients/connect_rest_client.py
+++ chats/apps/api/v1/internal/rest_clients/connect_rest_client.py
@@ -11,16 +11,26 @@ class ConnectRESTClient(InternalAuthentication):
         self.base_url = settings.CONNECT_API_URL
 
     def create_ticketer(self, **kwargs):
+        url = f"{self.base_url}/v1/organization/project/create_ticketer/"
+        if settings.USE_CONNECT_V2:
+            project_uuid = kwargs.pop("project_uuid")
+            url = f"{self.base_url}/v2/projects/{project_uuid}/ticketer/"
         response = requests.post(
-            url=f"{self.base_url}/v1/organization/project/create_ticketer/",
+            url=url,
             headers=self.headers,
             json={**kwargs, "ticketer_type": settings.FLOWS_TICKETER_TYPE},
         )
         return response
 
     def get_user_project_token(self, project, user_email: str):
-        url = f"{self.base_url}/v1/organization/project/user_api_token/"
-        params = {"user": user_email, "project_uuid": str(project.uuid)}
+        params = {"user": user_email}
+        project_uuid = str(project.uuid)
+        if settings.USE_CONNECT_V2:
+            url = f"{self.base_url}/v2/projects/{project_uuid}/user_api_token/"
+        else:
+            url = f"{self.base_url}/v1/organization/project/user_api_token/"
+            params["project_uuid"] = project_uuid
+
         response = requests.get(
             url=url,
             headers=self.headers,
--- chats/apps/api/v1/projects/viewsets.py
+++ chats/apps/api/v1/projects/viewsets.py
@@ -7,7 +7,6 @@ from rest_framework.response import Response
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 
-
 from chats.apps.api.v1.projects.serializers import (
     ProjectSerializer,
     ProjectFlowStartSerializer,
