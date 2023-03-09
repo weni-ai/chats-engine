cdbe0bad9b5168b4e0ced9788071401bd3f3fe9a
Helder Souza <42891390+helllllllder@users.noreply.github.com>
2023-03-09 17:53:53 -0300

    feat: Agent consumer ping method (#145)

--- chats/apps/api/websockets/tests/test_agent.py
+++ chats/apps/api/websockets/tests/test_agent.py
@@ -7,34 +7,41 @@ from chats.apps.contacts.models import Contact
 from chats.apps.projects.models import Project
 from chats.apps.rooms.models import Room
 from chats.apps.sectors.models import Sector, SectorAuthorization
+from channels.routing import URLRouter
+from chats.apps.api.websockets.rooms.routing import websocket_urlpatterns
+from chats.apps.accounts.authentication.channels.middleware import TokenAuthMiddleware
 
 
 class AgentConsumerTestCase(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
+        self.application = TokenAuthMiddleware(URLRouter(websocket_urlpatterns))
 
-        self.user, self.Token = create_user_and_token(nickname="agent")
+        self.user, self.token = create_user_and_token(nickname="agent")
         self.project = Project.objects.create(name="Test chat Project 1")
+        self.user_permission = self.project.permissions.create(user=self.user, role=1)
         self.sector = Sector.objects.create(
             name="Test chat Sector 1",
             project=self.project,
             rooms_limit=3,
-            work_start=7,
-            work_end=17,
+            work_start="07:00",
+            work_end="17:00",
         )
         self.sector_permission = SectorAuthorization.objects.create(
-            sector=self.sector, user=self.user, role=1
+            sector=self.sector, permission=self.user_permission, role=1
         )
+        self.queue = self.sector.queues.create(name="Test chat queue 1")
         self.contact = Contact.objects.create(
             name="Contact test 123", email="test@user.com"
         )
         self.room = Room.objects.create(
-            sector=self.sector, contact=self.contact, is_active=True
+            queue=self.queue, contact=self.contact, is_active=True
         )
 
     async def test_authenticate_ok(self):
         agent_comunicator = WebsocketCommunicator(
-            AgentRoomConsumer.as_asgi(), f"/ws/agent/rooms?Token={self.Token.pk}"
+            self.application,
+            f"/ws/agent/rooms?Token={self.token.pk}&project={self.project.pk}",
         )
         connected, subprotocol = await agent_comunicator.connect()
         self.assertTrue(connected)
