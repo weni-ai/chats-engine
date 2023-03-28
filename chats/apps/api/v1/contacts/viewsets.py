e6fb10acdcddaf2125e46f4d0e887287ee2dc4a1
helllllllder <heldergsds@gmail.com>
2023-03-28 11:54:45 -0300

    feat: Use user permission queue_ids when filtering the contacts to reduce the join count on the query

--- chats/apps/api/v1/contacts/viewsets.py
+++ chats/apps/api/v1/contacts/viewsets.py
@@ -16,35 +16,6 @@ class ContactViewset(viewsets.ReadOnlyModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
     search_fields = ["name"]
 
-    def get_queryset(self):
-        qs = self.queryset
-        user = self.request.user
-        is_queue_agent = Q(rooms__queue__authorizations__permission__user=user)
-
-        is_sector_manager = Q(
-            rooms__queue__sector__authorizations__permission__user=user
-        )
-
-        is_project_admin = Q(
-            Q(rooms__queue__sector__project__permissions__user=user)
-            & Q(rooms__queue__sector__project__permissions__role=1)
-        )
-
-        is_user_assigned_to_room = Q(rooms__user=user)
-
-        check_admin_manager_agent_role_filter = (
-            is_queue_agent
-            | is_sector_manager
-            | is_project_admin
-            | is_user_assigned_to_room
-        )
-
-        user_role_related_contacts = qs.filter(
-            check_admin_manager_agent_role_filter, rooms__is_active=False
-        ).distinct()
-
-        return user_role_related_contacts
-
     def retrieve(self, request, *args, **kwargs):
         contact = self.get_object()
         contact.can_retrieve(request.user, request.query_params.get("project"))
