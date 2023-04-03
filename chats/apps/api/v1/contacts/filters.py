96946df2647c0140c465627ccb3fe7831a0dd131
Helder Souza <42891390+helllllllder@users.noreply.github.com>
2023-04-03 17:38:02 -0300

    feat: Add ordering in the contactviewset and update the filter query (#176)

--- chats/apps/api/v1/contacts/filters.py
+++ chats/apps/api/v1/contacts/filters.py
@@ -1,7 +1,8 @@
 from django.utils.translation import gettext_lazy as _
 from django_filters import rest_framework as filters
 from chats.apps.contacts.models import Contact
-from django.db.models import Q
+from chats.apps.rooms.models import Room
+from django.db.models import Q, Max, OuterRef, Subquery
 
 
 class ContactFilter(filters.FilterSet):
@@ -45,16 +46,17 @@ class ContactFilter(filters.FilterSet):
         user = self.request.user
         user_permission = user.project_permissions.get(project=value)
         queue_ids = user_permission.queue_ids
-        room_queue = Q(rooms__queue__in=queue_ids)
-        is_user_assigned_to_room = Q(rooms__user=user) & Q(
-            rooms__queue__sector__project=value
-        )
-        check_queues_and_user_filter = room_queue | is_user_assigned_to_room
 
-        user_role_related_contacts = qs.filter(
-            check_queues_and_user_filter, rooms__is_active=False
-        ).distinct()
-        return user_role_related_contacts
+        subquery = Room.objects.filter(
+            Q(queue_id__in=queue_ids) | Q(user=user, queue__sector__project=value),
+            is_active=False,
+            contact_id=OuterRef("uuid"),
+        ).order_by("-ended_at")
+
+        queryset = qs.annotate(
+            last_ended_at=Subquery(subquery.values("ended_at")[:1])
+        ).filter(last_ended_at__isnull=False)
+        return queryset
 
     def filter_sector(self, queryset, name, value):
         return queryset.filter(rooms__queue__sector__uuid=value)
