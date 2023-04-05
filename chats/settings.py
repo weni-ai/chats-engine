f57369c8b5be86f14a01dc74955d3d4647795b3b
helllllllder <heldergsds@gmail.com>
2023-04-05 14:34:58 -0300

    feat: add urn to contact and rooms search, change search to use full text search

--- chats/settings.py
+++ chats/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.postgres",
     # chats apps
     "chats.apps.accounts",
     "chats.apps.contacts",
