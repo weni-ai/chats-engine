bbb38f793d82f604904c1cd884740086d6250b0b
helllllllder <heldergsds@gmail.com>
2023-05-04 16:08:37 -0300

    feat: Add tests

--- chats/apps/api/v1/rooms/filters.py
+++ chats/apps/api/v1/rooms/filters.py
@@ -1,9 +1,10 @@
+from django.contrib.auth import get_user_model
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q
 from django.utils.translation import gettext_lazy as _
 from django_filters import rest_framework as filters
+
 from chats.apps.rooms.models import Room
-from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
