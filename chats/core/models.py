19bdf9ab711846ce2ba79baae813d4d9f5b32eb0
AlanJaeger <aland295@gmail.com>
2023-05-19 14:30:20 -0300

    feature: concatenate name with deletation date

--- chats/core/models.py
+++ chats/core/models.py
@@ -6,7 +6,6 @@ from django.utils.translation import gettext_lazy as _
 
 
 class BaseModel(models.Model):
-
     uuid = models.UUIDField(default=uuid.uuid4, primary_key=True)
     created_on = models.DateTimeField(
         _("Created on"), editable=False, auto_now_add=True
@@ -22,14 +21,16 @@ class BaseModel(models.Model):
 
 
 class BaseSoftDeleteModel(models.Model):
-
     is_deleted = models.BooleanField(_("is deleted?"), default=False)
 
     class Meta:
         abstract = True
 
+    def deleted_sufix(self):
+        name_sufix = "_is_deleted_" + str(timezone.now())
+        return name_sufix
+
     def delete(self):
-        deleted_time = str(timezone.now())
         self.is_deleted = True
-        self.name = self.name + "_is_deleted_" + deleted_time
+        self.name += self.deleted_sufix()
         self.save()
