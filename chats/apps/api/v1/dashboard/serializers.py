5d5ed9cfaa8bdd1750b27c92225bfeabe2c7d4d9
helllllllder <heldergsds@gmail.com>
2023-05-31 12:47:22 -0300

    feat: Add user__isnull false to the agents room filter

--- chats/apps/api/v1/dashboard/serializers.py
+++ chats/apps/api/v1/dashboard/serializers.py
@@ -12,8 +12,8 @@ from django.utils import timezone
 from django_redis import get_redis_connection
 from rest_framework import serializers
 
-from chats.apps.rooms.models import Room
 from chats.apps.dashboard.models import RoomMetrics
+from chats.apps.rooms.models import Room
 
 
 def dashboard_general_data(context: dict, project):
@@ -82,7 +82,7 @@ def dashboard_general_data(context: dict, project):
 def dashboard_agents_data(context, project):
     initial_datetime = timezone.now().replace(hour=0, minute=0, second=0, microsecond=0)
 
-    rooms_filter = {}
+    rooms_filter = {"user__isnull": False}
     closed_rooms = {}
 
     if context.get("start_date") and context.get("end_date"):
@@ -103,6 +103,10 @@ def dashboard_agents_data(context, project):
     else:
         rooms_filter["queue__sector__project"] = project
 
+    agents_query = Room.objects
+    if not context.get("is_weni_admin"):
+        agents_query = agents_query.exclude(user__email__endswith="weni.ai")
+
     agents_query = (
         Room.objects.filter(**rooms_filter)
         .values("user")
@@ -112,9 +116,6 @@ def dashboard_agents_data(context, project):
             opened_rooms=Count("uuid", filter=Q(is_active=True)),
         )
     )
-
-    if not context.get("is_weni_admin"):
-        return agents_query.exclude(user__email__endswith="weni.ai")
 
     return agents_query
 
