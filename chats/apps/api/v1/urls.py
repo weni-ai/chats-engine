3cd90ce62c6268f5d7cb5f25ec7097ea4cec0570
AlanJaeger <aland295@gmail.com>
2023-06-05 16:33:44 -0300

    feature: removing metrics view in urls

--- chats/apps/api/v1/urls.py
+++ chats/apps/api/v1/urls.py
@@ -1,9 +1,7 @@
 from django.urls import include, path
 
-from chats.apps.api.v1.prometheus.views import metrics_view
 from chats.apps.api.v1.routers import router
 
 urlpatterns = [
     path("", include(router.urls)),
-    path("metrics/", metrics_view, name="metrics_view"),
 ]
