31766f832a3b4da3ea4c20de2e5d1c9713443f71
Helder Souza <42891390+helllllllder@users.noreply.github.com>
2023-06-07 18:16:09 -0300

    Feature/add apm (#228)
    * feat: add apm settings
    
    * feat: move apm code above keycloaks

--- chats/settings.py
+++ chats/settings.py
@@ -241,6 +241,21 @@ LOGGING["handlers"]["console"] = {
 }
 
 
+# Elastic APM
+
+USE_APM = env.bool("USE_APM", default=False)
+
+if USE_APM:
+    INSTALLED_APPS.append("elasticapm.contrib.django")
+
+    ELASTIC_APM = {
+        "SERVICE_NAME": env("APM_SERVICE_NAME", default="chats-production"),
+        "SECRET_TOKEN": env("APM_SECRET_TOKEN"),
+        "SERVER_URL": env("APM_SERVER_URL"),
+        "ENVIRONMENT": env("APM_SERVICE_ENVIRONMENT", default="production"),
+        "DEBUG": env.bool("APM_SERVICE_DEBUG", default=False),
+    }
+
 # mozilla-django-oidc
 
 OIDC_ENABLED = env.bool("OIDC_ENABLED", default=False)
