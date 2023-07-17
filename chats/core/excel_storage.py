4c2a5709fdb6a5b0ffe80f5f7750b440a5d1c834
AlanJaeger <aland295@gmail.com>
2023-07-17 14:32:30 -0300

    feature: resolving bugs in excel exporter from dashboard.

---
+++ chats/core/excel_storage.py
@@ -0,0 +1,8 @@
+from storages.backends.s3boto3 import S3Boto3Storage
+
+
+class ExcelStorage(S3Boto3Storage):  # pragma: no cover
+    def get_default_settings(self):
+        default_settings = super().get_default_settings()
+        default_settings["location"] = f"dashboard_data/excel"
+        return default_settings
