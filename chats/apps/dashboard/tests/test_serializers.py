70e4a65fa8e98f277ff63be3b6f7a031f7351b62
AlanJaeger <aland295@gmail.com>
2023-07-26 17:39:20 -0300

    feature: resolving duplicity in unit tests

--- chats/apps/dashboard/tests/test_serializers.py
+++ chats/apps/dashboard/tests/test_serializers.py
@@ -1,10 +1,8 @@
 from django.test import TestCase
-
 from chats.apps.api.v1.dashboard.serializers import (
     dashboard_general_data,
     dashboard_agents_data,
 )
-
 from chats.apps.projects.models import Project
 
 
@@ -38,31 +36,6 @@ class SerializerTests(TestCase):
         )
         self.assertEqual(serializer["active_chats"], 1)
 
-    def test_returned_fields_from_dashboard_agent_serializer(self):
-        project = Project.objects.get(uuid="34a93b52-231e-11ed-861d-0242ac120002")
-        instance = dashboard_agents_data(
-            project=project,
-            context={"is_weni_admin": True},
-        )
-        self.assertEqual(list(instance[0].keys())[0], "first_name")
-        self.assertEqual(list(instance[0].keys())[1], "email")
-        self.assertEqual(list(instance[0].keys())[2], "agent_status")
-        self.assertEqual(list(instance[0].keys())[3], "closed_rooms")
-        self.assertEqual(list(instance[0].keys())[4], "opened_rooms")
-
-    def test_field_value_from_dashboard_agent_serializer(self):
-        project = Project.objects.get(uuid="34a93b52-231e-11ed-861d-0242ac120002")
-        instance = dashboard_agents_data(
-            project=project,
-            context={"is_weni_admin": True},
-        )
-
-        self.assertEqual(instance[0]["first_name"], "")
-        self.assertEqual(instance[0]["email"], "amywong@chats.weni.ai")
-        self.assertEqual(instance[0]["agent_status"], "OFFLINE")
-        self.assertEqual(instance[0]["closed_rooms"], 0)
-        self.assertEqual(instance[0]["opened_rooms"], 1)
-        
     def test_returned_fields_from_dashboard_agent_serializer(self):
         project = Project.objects.get(uuid="34a93b52-231e-11ed-861d-0242ac120002")
         instance = dashboard_agents_data(
