4b535b6fbd1f883c57f3af2b32efb4f00578478a
helllllllder <heldergsds@gmail.com>
2023-07-26 17:18:25 -0300

    feat: add debug task

--- chats/celery.py
+++ chats/celery.py
@@ -15,3 +15,8 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print("Request: {0!r}".format(self.request))
