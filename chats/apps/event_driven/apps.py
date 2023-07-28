d52a99e96525603c71c4afebd4e9ed252aacafcb
helllllllder <heldergsds@gmail.com>
2023-07-28 09:03:25 -0300

    feat: add pyampq lib

--- chats/apps/event_driven/apps.py
+++ chats/apps/event_driven/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class EventDrivenConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "chats.event_driven"
+    name = "chats.apps.event_driven"
