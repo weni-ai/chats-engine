a92cc7a748059d074c0bf31af92500184472d45d
helllllllder <heldergsds@gmail.com>
2023-08-15 14:09:26 -0300

    feat: return none when facing errors on the project creation use case class, as the error handling code is not done

---
+++ chats/apps/event_driven/backends/pyamqp_backend.py
@@ -0,0 +1,48 @@
+import time
+
+import amqp
+from django.conf import settings
+
+
+class PyAMQPConnectionBackend:
+    _start_message = "[+] Connection established. Waiting for events"
+
+    def __init__(self, handle_consumers: callable):
+        self._handle_consumers = handle_consumers
+        self.connection_params = dict(
+            host=settings.EDA_BROKER_HOST,
+            port=settings.EDA_BROKER_PORT,
+            userid=settings.EDA_BROKER_USER,
+            password=settings.EDA_BROKER_PASSWORD,
+            virtual_host=settings.EDA_VIRTUAL_HOST,
+        )
+
+    def _drain_events(self, connection: amqp.connection.Connection):
+        while True:
+            connection.drain_events()
+
+    def start_consuming(self):
+        while True:
+            try:
+                with amqp.Connection(**self.connection_params) as connection:
+                    channel = connection.channel()
+
+                    self._handle_consumers(channel)
+
+                    print(self._start_message)
+
+                    self._drain_events(connection)
+
+            except (
+                amqp.exceptions.AMQPError,
+                ConnectionRefusedError,
+                OSError,
+            ) as error:
+                print(f"[-] Connection error: {error}")
+                print("    [+] Reconnecting in 5 seconds...")
+                time.sleep(5)
+
+            except Exception as error:
+                # TODO: Handle exceptions with RabbitMQ
+                print("error on drain_events:", type(error), error)
+                time.sleep(5)
