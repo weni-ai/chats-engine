0418434aa8e55d02d1ad3e8232309704e0482cea
AlanJaeger <aland295@gmail.com>
2023-09-05 16:23:11 -0300

    feature: removing prints

--- chats/apps/event_driven/base_app.py
+++ chats/apps/event_driven/base_app.py
@@ -6,7 +6,6 @@ class EventDrivenAPP:
     def __init__(self) -> None:
         handle_consumers_function = import_string(settings.EDA_CONSUMERS_HANDLE)
         connection_backend = import_string(settings.EDA_CONNECTION_BACKEND)
-        print("conection", connection_backend)
         self.connection_params = dict(
             host=settings.EDA_BROKER_HOST,
             port=settings.EDA_BROKER_PORT,
