11ee4858b39543fa5cc0860b9401b8e0b4c73bd4
AlanJaeger <aland295@gmail.com>
2023-09-14 17:55:12 -0300

    feature: changing variable name to body in template type consumer

--- chats/apps/projects/consumers/template_type_consumer.py
+++ chats/apps/projects/consumers/template_type_consumer.py
@@ -7,8 +7,6 @@ from chats.apps.event_driven.parsers.json_parser import JSONParser
 from chats.apps.projects.usecases import TemplateTypeCreation
 
 
-# TODO: use commented code, it's commented because we need to test only the
-# create method and other modules don't follow this message structure
 class TemplateTypeConsumer(EDAConsumer):
     @staticmethod
     @pyamqp_call_dlx_when_error(
@@ -17,11 +15,9 @@ class TemplateTypeConsumer(EDAConsumer):
     )
     def consume(message: amqp.Message):
         print(f"[TemplateTypeConsumer] - Consuming a message. Body: {message.body}")
-        notification = JSONParser.parse(message.body)
+        body = JSONParser.parse(message.body)
 
-        content = notification  # notification.get("content")
-
-        template_type_creation = TemplateTypeCreation(config=content)
+        template_type_creation = TemplateTypeCreation(config=body)
         template_type_creation.create()
 
         message.channel.basic_ack(message.delivery_tag)
