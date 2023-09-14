5d18842a8bb9e001e2e27683c6e8bcfddccadf3d
AlanJaeger <aland295@gmail.com>
2023-09-14 19:07:11 -0300

    feature: instancing template type class and calling handle

--- chats/apps/projects/handle.py
+++ chats/apps/projects/handle.py
@@ -4,4 +4,6 @@ from .consumers import TemplateTypeConsumer
 
 
 def handle_consumers(channel: Channel) -> None:
-    channel.basic_consume("chats.template-types", callback=TemplateTypeConsumer.handle)
+    channel.basic_consume(
+        "chats.template-types", callback=TemplateTypeConsumer().handle
+    )
