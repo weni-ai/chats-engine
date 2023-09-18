48a778b5838bf39fef6a52f13e37dac1196e9c58
AlanJaeger <aland295@gmail.com>
2023-09-18 17:03:22 -0300

    feature: EDA project consumer

--- chats/apps/projects/handle.py
+++ chats/apps/projects/handle.py
@@ -1,9 +1,11 @@
 from amqp.channel import Channel
 
-from .consumers import TemplateTypeConsumer
+from .consumers import TemplateTypeConsumer, ProjectConsumer, DeadLetterConsumer
 
 
 def handle_consumers(channel: Channel) -> None:
     channel.basic_consume(
         "chats.template-types", callback=TemplateTypeConsumer().handle
     )
+    channel.basic_consume("chats.projects", callback=ProjectConsumer().handle)
+    channel.basic_consume("chats.dlq", callback=DeadLetterConsumer.consume)
