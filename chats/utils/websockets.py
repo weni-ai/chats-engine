cd65b6f5a5190e2768b9d66f856e3b57213c5eb6
helllllllder <heldergsds@gmail.com>
2023-09-21 16:00:44 -0300

    feat: capture exceptions on the send channels group function for it not to break the code but send the error to sentry

--- chats/utils/websockets.py
+++ chats/utils/websockets.py
@@ -3,18 +3,22 @@ import json
 from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
 from django.core.serializers.json import DjangoJSONEncoder
+from sentry_sdk import capture_exception
 
 
 def send_channels_group(group_name: str, call_type: str, content: str, action: str):
     """
     helper function that sends data to channels groups
     """
-    channel_layer = get_channel_layer()
-    async_to_sync(channel_layer.group_send)(
-        group_name,
-        {
-            "type": call_type,
-            "action": action,
-            "content": json.dumps(content, cls=DjangoJSONEncoder),
-        },
-    )
+    try:
+        channel_layer = get_channel_layer()
+        async_to_sync(channel_layer.group_send)(
+            group_name,
+            {
+                "type": call_type,
+                "action": action,
+                "content": json.dumps(content, cls=DjangoJSONEncoder),
+            },
+        )
+    except Exception as err:
+        capture_exception(err)
