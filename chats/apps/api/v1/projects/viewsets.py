cc9056752750f06695fd22bec366de0c5627d659
helllllllder <heldergsds@gmail.com>
2023-10-16 11:52:00 -0300

    feat: feedback refactor

--- chats/apps/api/v1/projects/viewsets.py
+++ chats/apps/api/v1/projects/viewsets.py
@@ -30,6 +30,7 @@ from chats.apps.projects.models import (
     ProjectPermission,
 )
 from chats.apps.rooms.models import Room
+from chats.apps.rooms.views import create_room_feedback_message
 
 
 class ProjectViewset(viewsets.ReadOnlyModelViewSet):
@@ -275,8 +276,11 @@ class ProjectViewset(viewsets.ReadOnlyModelViewSet):
         chats_flow_start.external_id = flow_start.get("uuid")
         chats_flow_start.name = flow_start.get("flow").get("name")
         chats_flow_start.save()
+        feedback = {"name": chats_flow_start.name}
         if chats_flow_start.room:
             room.notify_room("update")
+            # TODO create feedback fs feedback
+            create_room_feedback_message(room, feedback, method="fs")
         return Response(flow_start, status.HTTP_200_OK)
 
 
