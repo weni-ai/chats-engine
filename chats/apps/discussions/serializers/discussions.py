6612aa967a4992a35f58a859dcad4ecda5e7ccce
helllllllder <heldergsds@gmail.com>
2023-10-30 08:19:02 -0300

    feat: add filters and serializers

---
+++ chats/apps/discussions/serializers/discussions.py
@@ -0,0 +1,30 @@
+from django.utils.translation import gettext_lazy as _
+from rest_framework import serializers
+
+from ..models import Discussion
+from ..services import create_discussion_message_and_notify
+
+
+class RoomFlowSerializer(serializers.ModelSerializer):
+    initial_message = serializers.CharField(
+        required=True, write_only=True, allow_null=True
+    )
+
+    class Meta:
+        model = Discussion
+        fields = [
+            "uuid",
+            "room",
+            "queue",
+            "subject",
+        ]
+        read_only_fields = [
+            "uuid",
+        ]
+
+    def create(self, validated_data):
+        initial_message = validated_data.pop("initial_message")
+        discussion = super().create(validated_data)
+        discussion.notify("create")
+        create_discussion_message_and_notify(discussion, initial_message)
+        return discussion
--- chats/apps/discussions/services.py
+++ chats/apps/discussions/services.py
@@ -1,3 +1,13 @@
 def create_discussion_message_and_notify(discussion, message):
     msg = discussion.messages.create(user=discussion.user, text=message)
     msg.notify_discussion("create")
+
+
+def create_discussion_and_initial_message(
+    discussion_data: dict, create_function, *args, **kwargs
+):
+    msg_content = discussion_data.pop("initial_message")
+    discussion = create_function(discussion_data, *args, **kwargs)
+    discussion.notify("create")
+    create_discussion_message_and_notify(discussion, msg_content)
+    return discussion
