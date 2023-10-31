07c52fae4b69f733d98dadcba51cf0b9328bb96d
helllllllder <heldergsds@gmail.com>
2023-10-31 17:59:17 -0300

    feat: add endpoints actions for the discussion user models

--- chats/apps/discussions/views/discussion.py
+++ chats/apps/discussions/views/discussion.py
@@ -1,10 +1,12 @@
+from django.contrib.auth import get_user_model
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters, viewsets
+from rest_framework import filters, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from ..filters import DiscussionFilter
-from ..models import Discussion
+from ..models import Discussion, DiscussionUser
 from ..serializers import (
     DiscussionCreateSerializer,
     DiscussionDetailSerializer,
@@ -12,16 +14,44 @@ from ..serializers import (
     DiscussionUserListSerializer,
 )
 
+User = get_user_model()
+
 
 class DiscussionUserActionsMixin:
-    @action(
-        detail=True,
-        methods=["POST"],
-        url_name="add_agents",
-        serializer_class=DiscussionUserListSerializer,
-    )
-    def add_agents(self):
-        pass
+    @action(detail=True, methods=["POST"], url_name="add_agents", filterset_class=None)
+    def add_agents(self, request, *args, **kwargs):
+        try:
+            user = request.POST.get("user_email")
+            user = User.objects.get(email=user)
+            discussion = self.get_object()
+            added_agent = discussion.create_discussion_user(user)
+            return Response(
+                {
+                    "first_name": user.first_name,
+                    "last_name": user.last_name,
+                    "role": added_agent.role,
+                },
+                status=status.HTTP_201_CREATED,
+            )
+        except Exception as error:
+            return Response(
+                {"detail": f"{type(error)}: {error}"},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
+
+    @action(detail=True, methods=["GET"], url_name="list_agents", filterset_class=None)
+    def list_agents(self, request, *args, **kwargs):
+        discussion = self.get_object()
+
+        queryset = DiscussionUser.objects.filter(discussion=discussion)
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = DiscussionUserListSerializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = DiscussionUserListSerializer(queryset, many=True)
+        return Response(serializer.data)
 
 
 class DiscussionViewSet(viewsets.ModelViewSet, DiscussionUserActionsMixin):
@@ -31,8 +61,8 @@ class DiscussionViewSet(viewsets.ModelViewSet, DiscussionUserActionsMixin):
     permission_classes = [IsAuthenticated]
     lookup_field = "uuid"
 
-    def create(self, request, *args, **kwargs):
-        return super().create(request, *args, **kwargs)
+    def get_paginated_response(self, data):
+        return super().get_paginated_response(data)
 
     def get_serializer_class(self):
         if self.action == "create":
