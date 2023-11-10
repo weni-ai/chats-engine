cf7da967798c67d1dc5fbdb11c5dbf21eb900f93
nataliaweni <natalia.assis@weni.ai>
2023-11-10 18:45:16 -0300

    Modify dockerfile

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,3 +5,52 @@ python manage.py collectstatic --noinput
 
 echo "Starting server"
 exec gunicorn chats.asgi -c gunicorn.conf.py
+
+do_gosu(){
+    user="$1"
+    shift 1
+
+    is_exec="false"
+    if [ "$1" = "exec" ]; then
+        is_exec="true"
+        shift 1
+    fi
+
+    if [ "$(id -u)" = "0" ]; then
+        if [ "${is_exec}" = "true" ]; then
+            exec gosu "${user}" "$@"
+        else
+            gosu "${user}" "$@"
+            return "$?"
+        fi
+    else
+        if [ "${is_exec}" = "true" ]; then
+            exec "$@"
+        else
+            eval '"$@"'
+            return "$?"
+        fi
+    fi
+}
+
+
+if [[ "start" == "$1" ]]; then
+    do_gosu "${APP_USER}:${APP_GROUP}" exec gunicorn "${GUNICORN_APP}" \
+      --name="${APP_NAME}" \
+      --chdir="${APP_PATH}" \
+      --bind=0.0.0.0:8080 \
+      --log-config="${GUNICORN_LOG_CONF}" \
+      -c "${GUNICORN_CONF}"
+elif [[ "celery-worker" == "$1" ]]; then
+    celery_queue="celery"
+    if [ "${2}" ] ; then
+        celery_queue="${2}"
+    fi
+    do_gosu "${APP_USER}:${APP_GROUP}" exec celery \
+        -A "${CELERY_APP}" --workdir="${APP_PATH}" worker \
+        -Q "${celery_queue}" \
+        -O fair \
+        -l "${LOG_LEVEL}" \
+        --autoscale=4,1
+if
+exec "$@"
