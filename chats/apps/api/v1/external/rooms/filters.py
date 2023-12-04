bffa6a9bc37f7644a79f779b81533014a9c46644
helllllllder <heldergsds@gmail.com>
2023-12-04 15:23:33 -0300

    feat: remove user import on externa rooms filter

--- chats/apps/api/v1/external/rooms/filters.py
+++ chats/apps/api/v1/external/rooms/filters.py
@@ -1,9 +1,6 @@
-from django.contrib.auth import get_user_model
 from django_filters import rest_framework as filters
 
 from chats.apps.rooms.models import Room
-
-User = get_user_model()
 
 
 class RoomFilter(filters.FilterSet):
