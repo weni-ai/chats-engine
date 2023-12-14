c76741b783b15bfb28702bd59f6754ec6faa8dcf
nataliaweni <natalia.assis@weni.ai>
2023-12-14 18:26:14 -0300

    Fix dockerfile and docker-entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+export GUNICORN_APP=${GUNICORN_APP:-"app.wsgi:application"}
+export CELERY_APP=${CELERY_APP:-"app.wsgi:celery"}
+export GUNICORN_LOG_CONF=${GUNICORN_LOG_CONF:-"${PROJECT_PATH}/docker/gunicorn/gunicorn-logging.conf"}
+export GUNICORN_CONF=${GUNICORN_CONF:-"${PROJECT_PATH}/docker/gunicorn/gunicorn.conf.py"}
+export LOG_LEVEL=${LOG_LEVEL:-"INFO"}
+#export GUNICORN_CONF=${GUNICORN_CONF:-"python:app.gunicorn"
+export CELERY_MAX_WORKERS=${CELERY_MAX_WORKERS:-'4'}
+export CELERY_BEAT_DATABASE_FILE=${CELERY_BEAT_DATABASE_FILE:-'/tmp/celery_beat_database'}
+export HEALTHCHECK_TIMEOUT=${HEALTHCHECK_TIMEOUT:-"10"}
+
 echo "Running collectstatic"
 python manage.py collectstatic --noinput
 
@@ -37,7 +47,7 @@ do_gosu(){
 if [[ "start" == "$1" ]]; then
     do_gosu "${APP_USER}:${APP_GROUP}" exec gunicorn "${GUNICORN_APP}" \
       --name="${APP_NAME}" \
-      --chdir="${APP_PATH}" \
+      --chdir="${PROJECT_PATH}" \
       --bind=0.0.0.0:8080 \
       --log-config="${GUNICORN_LOG_CONF}" \
       -c "${GUNICORN_CONF}"
@@ -52,5 +62,20 @@ elif [[ "celery-worker" == "$1" ]]; then
         -O fair \
         -l "${LOG_LEVEL}" \
         --autoscale=4,1
-if
+elif [[ "healthcheck-celery-worker" == "$1" ]]; then
+    celery_queue="celery"
+    if [ "${2}" ] ; then
+        celery_queue="${2}"
+    fi
+    HEALTHCHECK_OUT=$(
+        do_gosu "${APP_USER}:${APP_GROUP}" celery -A "${CELERY_APP}" \
+            inspect ping \
+            -d "${celery_queue}@${HOSTNAME}" \
+            --timeout "${HEALTHCHECK_TIMEOUT}" 2>&1
+    )
+    echo "${HEALTHCHECK_OUT}"
+    grep -F -qs "${celery_queue}@${HOSTNAME}: OK" <<< "${HEALTHCHECK_OUT}" || exit 1
+    exit 0
+fi
+
 exec "$@"
