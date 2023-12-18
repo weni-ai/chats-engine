820de5001d0fb5926c9b2efb649082bb9ed8dc80
helllllllder <heldergsds@gmail.com>
2023-12-18 10:48:59 -0300

    feat: remove uvicorn workers from the gunicorn config file, as the asgi server will be run on daphne instead

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -4,7 +4,6 @@ import os
 workers = os.environ.get("GUNICORN_WORKERS", multiprocessing.cpu_count() * 2 + 1)
 proc_name = "chats"
 default_proc_name = proc_name
-# worker_class = "uvicorn.workers.UvicornWorker"
 accesslog = "gunicorn.access"
 timeout = 120
 bind = "0.0.0.0"
