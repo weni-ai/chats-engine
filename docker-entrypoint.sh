af171582ce6e41aebd6e976465f386af597578ca
helllllllder <heldergsds@gmail.com>
2023-12-29 09:30:37 -0300

    feat: fix entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 
-export GUNICORN_APP=${GUNICORN_APP:-"app.wsgi:application"}
-export CELERY_APP=${CELERY_APP:-"app.wsgi:celery"}
-export GUNICORN_LOG_CONF=${GUNICORN_LOG_CONF:-"${PROJECT_PATH}/docker/gunicorn/gunicorn-logging.conf"}
+export GUNICORN_APP=${GUNICORN_APP:-"chats.asgi"}
+export CELERY_APP=${CELERY_APP:-"chats"}
 export GUNICORN_CONF=${GUNICORN_CONF:-"${PROJECT_PATH}/docker/gunicorn/gunicorn.conf.py"}
 export LOG_LEVEL=${LOG_LEVEL:-"INFO"}
-#export GUNICORN_CONF=${GUNICORN_CONF:-"python:app.gunicorn"
-export CELERY_MAX_WORKERS=${CELERY_MAX_WORKERS:-'4'}
-export CELERY_BEAT_DATABASE_FILE=${CELERY_BEAT_DATABASE_FILE:-'/tmp/celery_beat_database'}
+export CELERY_MAX_WORKERS=${CELERY_MAX_WORKERS:-'6'}
+# export CELERY_BEAT_DATABASE_FILE=${CELERY_BEAT_DATABASE_FILE:-'/tmp/celery_beat_database'}
 export HEALTHCHECK_TIMEOUT=${HEALTHCHECK_TIMEOUT:-"10"}
-
-echo "Running collectstatic"
-python manage.py collectstatic --noinput
-
-echo "Starting server"
-exec gunicorn chats.asgi -c gunicorn.conf.py
 
 do_gosu(){
     user="$1"
@@ -45,30 +37,32 @@ do_gosu(){
 
 
 if [[ "start" == "$1" ]]; then
-    do_gosu "${APP_USER}:${APP_GROUP}" exec gunicorn "${GUNICORN_APP}" \
-      --name="${APP_NAME}" \
+    do_gosu "${PROJECT_USER}:${PROJECT_GROUP}" python manage.py collectstatic --noinput
+    do_gosu "${PROJECT_USER}:${PROJECT_GROUP}" exec gunicorn "${GUNICORN_APP}" \
+      --name="${APPLICATION_NAME}" \
       --chdir="${PROJECT_PATH}" \
-      --bind=0.0.0.0:8080 \
-      --log-config="${GUNICORN_LOG_CONF}" \
+      --bind=0.0.0.0:8000 \
       -c "${GUNICORN_CONF}"
+elif [[ "start-websocket" == "$1" ]]; then
+    do_gosu "${PROJECT_USER}:${PROJECT_GROUP}" exec daphne -b 0.0.0.0 -p 8000 chats.asgi:application
 elif [[ "celery-worker" == "$1" ]]; then
     celery_queue="celery"
     if [ "${2}" ] ; then
         celery_queue="${2}"
     fi
-    do_gosu "${APP_USER}:${APP_GROUP}" exec celery \
-        -A "${CELERY_APP}" --workdir="${APP_PATH}" worker \
+    do_gosu "${PROJECT_USER}:${PROJECT_GROUP}" exec celery \
+        -A "${CELERY_APP}" --workdir="${PROJECT_PATH}" worker \
         -Q "${celery_queue}" \
         -O fair \
         -l "${LOG_LEVEL}" \
-        --autoscale=4,1
+        --autoscale=${CELERY_MAX_WORKERS},1
 elif [[ "healthcheck-celery-worker" == "$1" ]]; then
     celery_queue="celery"
     if [ "${2}" ] ; then
         celery_queue="${2}"
     fi
     HEALTHCHECK_OUT=$(
-        do_gosu "${APP_USER}:${APP_GROUP}" celery -A "${CELERY_APP}" \
+        do_gosu "${PROJECT_USER}:${PROJECT_GROUP}" celery -A "${CELERY_APP}" \
             inspect ping \
             -d "${celery_queue}@${HOSTNAME}" \
             --timeout "${HEALTHCHECK_TIMEOUT}" 2>&1
