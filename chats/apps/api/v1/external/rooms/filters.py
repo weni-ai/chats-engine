a25cc20ab0099f8dca1ebe9a8aa20838dea6cee2
helllllllder <heldergsds@gmail.com>
2024-02-26 10:23:32 -0300

    feat: filter rooms by sector

--- chats/apps/api/v1/external/rooms/filters.py
+++ chats/apps/api/v1/external/rooms/filters.py
@@ -1,9 +1,20 @@
+from django.db.models import Q
 from django_filters import rest_framework as filters
 
 from chats.apps.rooms.models import Room
 
 
 class RoomFilter(filters.FilterSet):
+    sector = filters.CharFilter(
+        field_name="sector",
+        required=False,
+        method="filter_sector",
+    )
+
     class Meta:
         model = Room
-        fields = ["urn", "is_active", "sector", "sector__name"]
+        fields = ["urn", "is_active", "sector"]
+
+    def filter_sector(self, queryset, name, value):
+        sector_filter = Q(queue__sector__uuid=value) | Q(queue__sector__name=value)
+        return queryset.filter(sector_filter)
