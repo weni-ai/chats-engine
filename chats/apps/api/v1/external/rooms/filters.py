ff778c4c3e7593da620a9d4296cfffa525c1879b
Helder Souza <42891390+helllllllder@users.noreply.github.com>
2024-02-26 16:19:38 -0300

    Feature/external rooms filter sector (#343)
    * feat: add add sector filter to the rooms list external endpoint
    
    * feat: filter rooms by sector
    
    * feat: remove sector reference from search fields on the external list rooms endpoint
    
    * feat: use icontains to treat uuid as a string

--- chats/apps/api/v1/external/rooms/filters.py
+++ chats/apps/api/v1/external/rooms/filters.py
@@ -1,9 +1,22 @@
+from django.db.models import Q
 from django_filters import rest_framework as filters
 
 from chats.apps.rooms.models import Room
 
 
 class RoomFilter(filters.FilterSet):
+    sector = filters.CharFilter(
+        field_name="sector",
+        required=False,
+        method="filter_sector",
+    )
+
     class Meta:
         model = Room
-        fields = ["urn", "is_active"]
+        fields = ["urn", "is_active", "sector"]
+
+    def filter_sector(self, queryset, name, value):
+        sector_filter = Q(queue__sector__uuid__icontains=value) | Q(
+            queue__sector__name__icontains=value
+        )
+        return queryset.filter(sector_filter)
