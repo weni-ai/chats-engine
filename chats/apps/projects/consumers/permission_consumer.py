5e4cb0cc4c7aa0a56a6731ae897c37c43a67333a
AlanJaeger <aland295@gmail.com>
2024-03-20 15:10:03 -0300

    feature: new consumer to project permissions

--- chats/apps/projects/consumers/permission_consumer.py
+++ chats/apps/projects/consumers/permission_consumer.py
@@ -3,3 +3,37 @@ from django.conf import settings
 
 from chats.apps.event_driven.consumers import EDAConsumer, pyamqp_call_dlx_when_error
 from chats.apps.event_driven.parsers.json_parser import JSONParser
+from chats.apps.projects.usecases.permission_creation import (
+    ProjectPermissionDTO,
+    ProjectPermissionCreationUseCase,
+)
+
+
+class ProjectPermissionConsumer(EDAConsumer):
+    @staticmethod
+    @pyamqp_call_dlx_when_error(
+        default_exchange=settings.CONNECT_DEFAULT_DEAD_LETTER_EXCHANGE,
+        routing_key="",
+        consumer_name="ProjectPermissionConsumer",
+    )
+    def consume(message: amqp.Message):
+        channel = message.channel
+        print(
+            f"[ProjectPermissionConsumer] - Consuming a message. Body: {message.body}"
+        )
+        body = JSONParser.parse(message.body)
+
+        project_permission_dto = ProjectPermissionDTO(
+            uuid=body.get("uuid"),
+            project=body.get("project"),
+            user=body.get("user"),
+            role=body.get("role"),
+        )
+        project_permission = ProjectPermissionCreationUseCase(config=body)
+
+        if body.get("action") == "delete":
+            project_permission.delete_permission(project_permission_dto)
+        elif body.get("action") == "create" or body.get("action") == "update":
+            project_permission.create_permission(project_permission_dto)
+
+        channel.basic_ack(message.delivery_tag)
--- chats/apps/projects/usecases/permission_creation.py
+++ chats/apps/projects/usecases/permission_creation.py
@@ -3,13 +3,13 @@ from dataclasses import dataclass
 from django.contrib.auth import get_user_model
 
 from ..models import ProjectPermission
-from .exceptions import InvalidProjectData
+from .exceptions import InvalidProjectPermission
 
 User = get_user_model()
 
 
 @dataclass
-class ProjectPermissionCreationDTO:
+class ProjectPermissionDTO:
     uuid: str
     project: str
     user: str
@@ -17,13 +17,36 @@ class ProjectPermissionCreationDTO:
 
 
 class ProjectPermissionCreationUseCase:
-    def create_permission(self, project_permission_dto: ProjectPermissionCreationDTO):
+    def __init__(self, config: dict) -> None:
+        self.config = config
 
-        # verificar se ja existe
+    def get_or_create_user_by_email(self, email: str) -> tuple:
+        return User.objects.get_or_create(email=email)
+
+    def edit_permission(self, project_permission: ProjectPermission, user):
+        project_permission.project = self.config.get("project")
+        project_permission.user = user
+        project_permission.role = self.config.get("role")
+        project_permission.save()
+
+    def create_permission(self, project_permission_dto: ProjectPermissionDTO):
+        user, _ = self.get_or_create_user_by_email(project_permission_dto.user)
+
+        project_permission = ProjectPermission.objects.get(
+            uuid=self.config.get("uuid"), default=None
+        )
+
+        if project_permission is not None:
+            self.edit_permission(project_permission, user)
 
         project_permission = ProjectPermission.objects.create(
-            uuid=project_permission_dto.uuid,
             project=project_permission_dto.project,
             user=project_permission_dto.user,
             role=project_permission_dto.role,
         )
+
+    def delete_permission(self, project_permission_dto: ProjectPermissionDTO):
+        project_permission = ProjectPermission.objects.get(
+            uuid=project_permission_dto.uuid
+        )
+        project_permission.delete()
