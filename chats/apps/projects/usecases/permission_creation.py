b1570c070e19215be82cfdd6e5fdfb5ca98ecbe0
AlanJaeger <aland295@gmail.com>
2024-03-26 16:48:24 -0300

    feature: passing project in create permission

--- chats/apps/projects/usecases/permission_creation.py
+++ chats/apps/projects/usecases/permission_creation.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 
 from django.contrib.auth import get_user_model
 
-from ..models import ProjectPermission
+from ..models import ProjectPermission, Project
 
 from chats.apps.projects.usecases import InvalidProjectPermission
 
@@ -23,11 +23,16 @@ class ProjectPermissionCreationUseCase:
     def role_mapping(self):
         return 1 if self.config.get("role") == 3 else 2
 
+    def get_project(self):
+        return Project.objects.get(uuid=self.config.get("project"))
+
     def get_or_create_user_by_email(self, email: str) -> tuple:
         return User.objects.get_or_create(email=email)
 
-    def edit_permission(self, project_permission: ProjectPermission, user, role_value):
-        project_permission.project = self.config.get("project")
+    def edit_permission(
+        self, project_permission: ProjectPermission, user, role_value, project
+    ):
+        project_permission.project = project
         project_permission.user = user
         project_permission.role = role_value
         project_permission.save()
@@ -35,6 +40,7 @@ class ProjectPermissionCreationUseCase:
     def create_permission(self, project_permission_dto: ProjectPermissionDTO):
         user, _ = self.get_or_create_user_by_email(project_permission_dto.user)
         role_value = self.role_mapping()
+        project = self.get_project()
 
         project_permission = ProjectPermission.objects.filter(
             project=project_permission_dto.project,
@@ -42,10 +48,10 @@ class ProjectPermissionCreationUseCase:
         ).first()
 
         if project_permission is not None:
-            self.edit_permission(project_permission, user, role_value)
+            self.edit_permission(project_permission, user, role_value, project)
 
         project_permission = ProjectPermission.objects.create(
-            project=project_permission_dto.project,
+            project=project,
             user=user,
             role=role_value,
         )
