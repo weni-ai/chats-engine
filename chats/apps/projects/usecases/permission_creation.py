fe54185dc452e2dcb85fe7dc370c7a7d1a3a806a
AlanJaeger <aland295@gmail.com>
2024-03-27 18:08:23 -0300

    feature: resolving bugs

--- chats/apps/projects/usecases/permission_creation.py
+++ chats/apps/projects/usecases/permission_creation.py
@@ -2,9 +2,9 @@ from dataclasses import dataclass
 
 from django.contrib.auth import get_user_model
 
-from ..models import ProjectPermission
-
 from chats.apps.projects.usecases import InvalidProjectPermission
+
+from ..models import Project, ProjectPermission
 
 User = get_user_model()
 
@@ -23,11 +23,16 @@ class ProjectPermissionCreationUseCase:
     def role_mapping(self):
         return 1 if self.config.get("role") == 3 else 2
 
+    def get_project(self):
+        return Project.objects.get(uuid=self.config.get("project"))
+
     def get_or_create_user_by_email(self, email: str) -> tuple:
         return User.objects.get_or_create(email=email)
 
-    def edit_permission(self, project_permission: ProjectPermission, user, role_value):
-        project_permission.project = self.config.get("project")
+    def edit_permission(
+        self, project_permission: ProjectPermission, user, role_value, project
+    ):
+        project_permission.project = project
         project_permission.user = user
         project_permission.role = role_value
         project_permission.save()
@@ -35,21 +40,14 @@ class ProjectPermissionCreationUseCase:
     def create_permission(self, project_permission_dto: ProjectPermissionDTO):
         user, _ = self.get_or_create_user_by_email(project_permission_dto.user)
         role_value = self.role_mapping()
+        project = self.get_project()
 
-        project_permission = ProjectPermission.objects.get(
-            project=project_permission_dto.project,
-            user=project_permission_dto.user,
-            default=None,
+        project_permission, created = ProjectPermission.objects.get_or_create(
+            project=project, user=user, defaults={"role": role_value}
         )
 
-        if project_permission is not None:
-            self.edit_permission(project_permission, user, role_value)
-
-        project_permission = ProjectPermission.objects.create(
-            project=project_permission_dto.project,
-            user=project_permission_dto.user,
-            role=role_value,
-        )
+        if not created:
+            self.edit_permission(project_permission, user, role_value, project)
 
     def delete_permission(self, project_permission_dto: ProjectPermissionDTO):
         try:
