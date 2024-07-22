13db714ff5fa55c635669c5c0f71f8dca6c917f1
alandavl <aland295@gmail.com>
2024-07-22 17:45:15 -0300

    feat: returning 400 if project uuid its not provided (#386)

--- chats/apps/api/v1/users/serializers.py
+++ chats/apps/api/v1/users/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 
 from chats.apps.accounts.models import Profile
 from chats.apps.projects.models.models import ProjectPermission
+from rest_framework.exceptions import APIException
 
 
 class ProfileSerializer(serializers.ModelSerializer):
@@ -12,6 +13,13 @@ class ProfileSerializer(serializers.ModelSerializer):
 
     def get_project_permission_role(self, obj):
         project_uuid = self.context.get("project_uuid")
+
+        if not project_uuid:
+            raise APIException(
+                "The 'project_uuid' parameter is required but was not provided.",
+                code=400,
+            )
+
         project_permission = ProjectPermission.objects.filter(
             project=project_uuid, user=obj.user
         ).first()
