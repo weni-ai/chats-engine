1188ae9e95126b7ccd6968ce47baa86bf316f584
alandavl <aland295@gmail.com>
2024-07-31 17:28:12 -0300

    Fix/ws message when transfer queue (#388)
    * feat: notify user when transfer queue

--- chats/apps/api/v1/rooms/viewsets.py
+++ chats/apps/api/v1/rooms/viewsets.py
@@ -395,6 +395,7 @@ class RoomViewset(
                     room.save()
 
                     create_room_feedback_message(room, feedback, method="rt")
+                    room.notify_user("update")
                     room.notify_queue("update")
 
         except Exception as error:
