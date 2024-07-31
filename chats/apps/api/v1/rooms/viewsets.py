dea507e6fcea587dc31fc2609d2a435b16caefff
AlanJaeger <aland295@gmail.com>
2024-07-31 18:45:17 -0300

    fix: passing user to ws notify

--- chats/apps/api/v1/rooms/viewsets.py
+++ chats/apps/api/v1/rooms/viewsets.py
@@ -395,7 +395,8 @@ class RoomViewset(
                     room.save()
 
                     create_room_feedback_message(room, feedback, method="rt")
-                    room.notify_user("update")
+                    user = User.objects.get(email=user_email)
+                    room.notify_user("update", user=user)
                     room.notify_queue("update")
 
         except Exception as error:
