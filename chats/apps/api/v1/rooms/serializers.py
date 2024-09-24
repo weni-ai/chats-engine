38452711c7e5be2b58160c4cc82172f5ae81ed55
helllllllder <heldergsds@gmail.com>
2024-09-24 15:50:52 -0300

    feat: refactor a little on roomviewset and serializer

--- chats/apps/api/v1/rooms/serializers.py
+++ chats/apps/api/v1/rooms/serializers.py
@@ -3,7 +3,10 @@ from rest_framework import serializers
 
 from chats.apps.accounts.models import User
 from chats.apps.api.v1.accounts.serializers import UserSerializer
-from chats.apps.api.v1.contacts.serializers import ContactRelationsSerializer
+from chats.apps.api.v1.contacts.serializers import (
+    ContactRelationsSerializer,
+    ContactSimpleSerializer,
+)
 from chats.apps.api.v1.queues.serializers import QueueSerializer
 from chats.apps.api.v1.sectors.serializers import DetailSectorTagSerializer
 from chats.apps.queues.models import Queue
@@ -39,6 +42,77 @@ class RoomSerializer(serializers.ModelSerializer):
         fields = "__all__"
         read_only_fields = [
             "created_on",
+            "ended_at",
+            "custom_fields",
+            "urn",
+            "linked_user",
+            "is_24h_valid",
+            "last_interaction",
+            "can_edit_custom_fields",
+        ]
+
+    def get_is_24h_valid(self, room: Room) -> bool:
+        return room.is_24h_valid
+
+    def get_flowstart_data(self, room: Room) -> bool:
+        try:
+            flowstart = room.flowstarts.get(is_deleted=False)
+        except (ObjectDoesNotExist, MultipleObjectsReturned):
+            return {}
+        return {
+            "name": flowstart.name,
+            "is_deleted": flowstart.is_deleted,
+            "created_on": flowstart.created_on,
+        }
+
+    def get_linked_user(self, room: Room):
+        try:
+            return room.contact.get_linked_user(room.queue.sector.project).full_name
+        except AttributeError:
+            return ""
+
+    def get_is_waiting(self, room: Room):
+        return room.get_is_waiting()
+
+    def get_unread_msgs(self, room: Room):
+        return room.messages.filter(seen=False).count()
+
+    def get_last_message(self, room: Room):
+        last_message = (
+            room.messages.order_by("-created_on")
+            .exclude(user__isnull=True, contact__isnull=True)
+            .exclude(text="")
+            .first()
+        )
+        return "" if last_message is None else last_message.text
+
+    def get_can_edit_custom_fields(self, room: Room):
+        return room.queue.sector.can_edit_custom_fields
+
+
+class ListRoomSerializer(serializers.ModelSerializer):
+    user = UserSerializer(many=False, read_only=True)
+    contact = ContactSimpleSerializer(many=False, read_only=True)
+    unread_msgs = serializers.SerializerMethodField()
+    last_message = serializers.SerializerMethodField()
+    is_waiting = serializers.SerializerMethodField()
+    linked_user = serializers.SerializerMethodField()
+    is_24h_valid = serializers.SerializerMethodField()
+    flowstart_data = serializers.SerializerMethodField()
+    last_interaction = serializers.DateTimeField(read_only=True)
+    can_edit_custom_fields = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Room
+        fields = [
+            "uuid",
+            "custom_fields",
+            "urn",
+            "transfer_history",
+            "protocol",
+            "service_chat",
+        ]
+        read_only_fields = [
             "ended_at",
             "custom_fields",
             "urn",
