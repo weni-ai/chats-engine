1f5f1f98ba043cb051b5a262e193c7cfcfbdbdcc
helllllllder <heldergsds@gmail.com>
2024-10-18 16:52:46 -0300

    feat: remove update on excelstorage

--- chats/core/excel_storage.py
+++ chats/core/excel_storage.py
@@ -2,11 +2,6 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class ExcelStorage(S3Boto3Storage):  # pragma: no cover
-    location = "private"
-    default_acl = "private"
-    file_overwrite = False
-    custom_domain = False
-
     def get_default_settings(self):
         default_settings = super().get_default_settings()
         default_settings["location"] = "dashboard_data/excel"
