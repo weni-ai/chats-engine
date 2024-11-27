f86f7287513ad3972cbd83a26a0609fdc2bf0594
AlanJaeger <aland295@gmail.com>
2024-11-27 15:54:44 -0300

    feature: passing project uuid as str

--- chats/apps/projects/usecases/send_room_info.py
+++ chats/apps/projects/usecases/send_room_info.py
@@ -9,7 +9,7 @@ class RoomInfoUseCase:
 
     def get_room(self, room: Room):
         room_dto = {
-            "project_uuid": room.project.uuid,
+            "project_uuid": str(room.project.uuid),
             "external_id": room.contact.external_id,
             "created_on": room.created_on.isoformat(),
         }
