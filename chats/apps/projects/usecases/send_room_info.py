cde3bdeeba9a59dcff7e7bbb3e010d185ba2250e
AlanJaeger <aland295@gmail.com>
2024-11-27 15:43:42 -0300

    feature: passing room dto as dict

--- chats/apps/projects/usecases/send_room_info.py
+++ chats/apps/projects/usecases/send_room_info.py
@@ -8,9 +8,9 @@ class RoomInfoUseCase:
         self._rooms_client = RoomsInfoMixin()
 
     def get_room(self, room: Room):
-        room_dto = RoomDTO(
-            project_uuid=room.project.uuid,
-            external_id=room.contact.external_id,
-            created_on=room.created_on,
-        )
+        room_dto = {
+            "project_uuid": room.project.uuid,
+            "external_id": room.contact.external_id,
+            "created_on": room.created_on.isoformat(),
+        }
         self._rooms_client.request_room(content=room_dto)
