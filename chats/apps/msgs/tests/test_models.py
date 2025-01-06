0b3ce1fcf9f6d978806a9b5fc1dbf3bee8d44345
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2025-01-06 16:36:42 -0300

    feat: add tests for MessageMediaModel creation with and without passing created_on

--- chats/apps/msgs/tests/test_models.py
+++ chats/apps/msgs/tests/test_models.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.utils import timezone
 from django.utils.timezone import timedelta
 
-from chats.apps.msgs.models import Message
+from chats.apps.msgs.models import Message, MessageMedia
 from chats.apps.rooms.models import Room
 
 
@@ -10,7 +10,7 @@ class TestMessageModel(TestCase):
     def setUp(self):
         self.room = Room.objects.create()
 
-    def test_message_passing_created_on(self):
+    def test_create_message_passing_created_on(self):
         timestamp = timezone.now() - timedelta(days=2)
 
         msg = Message.objects.create(room=self.room, created_on=timestamp)
@@ -20,7 +20,28 @@ class TestMessageModel(TestCase):
         self.assertEqual(msg.created_on.minute, timestamp.minute)
         self.assertEqual(msg.created_on.second, timestamp.second)
 
-    def test_message_without_passing_created_on(self):
+    def test_create_message_without_passing_created_on(self):
         msg = Message.objects.create(room=self.room)
 
         self.assertEqual(msg.created_on.date(), timezone.now().date())
+
+
+class TestMessageMediaModel(TestCase):
+    def setUp(self):
+        self.room = Room.objects.create()
+        self.msg = Message.objects.create(room=self.room)
+
+    def test_create_message_media_passing_created_on(self):
+        timestamp = timezone.now() - timedelta(days=2)
+
+        msg_media = MessageMedia.objects.create(message=self.msg, created_on=timestamp)
+
+        self.assertEqual(msg_media.created_on.date(), timestamp.date())
+        self.assertEqual(msg_media.created_on.hour, timestamp.hour)
+        self.assertEqual(msg_media.created_on.minute, timestamp.minute)
+        self.assertEqual(msg_media.created_on.second, timestamp.second)
+
+    def test_create_message_media_without_passing_created_on(self):
+        msg_media = MessageMedia.objects.create(message=self.msg)
+
+        self.assertEqual(msg_media.created_on.date(), timezone.now().date())
