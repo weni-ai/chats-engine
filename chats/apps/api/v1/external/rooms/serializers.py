ff2c6da6afda98442a26283fe3be234bbb445cb5
AlanJaeger <aland295@gmail.com>
2025-01-09 15:17:48 -0300

    feature: adding get_or_create to prevent the same room from being created two times

--- chats/apps/api/v1/external/rooms/serializers.py
+++ chats/apps/api/v1/external/rooms/serializers.py
@@ -198,7 +198,7 @@ class RoomFlowSerializer(serializers.ModelSerializer):
             contact, queue, user, groups, created, flow_uuid, project
         )
 
-        room = Room.objects.create(
+        room = Room.objects.get_or_create(
             **validated_data,
             project_uuid=str(queue.project.uuid),
             contact=contact,
