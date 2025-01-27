7ebc992909d7a541422d4c0e06dd874375da7daa
Eric Costa <ericosta.dev@gmail.com>
2025-01-27 15:50:05 -0300

    feature: add RoomDTO with JSON serialization method

--- chats/apps/api/v1/dto/room_dto.py
+++ chats/apps/api/v1/dto/room_dto.py
@@ -1,9 +1,13 @@
+import json
 from dataclasses import dataclass
-from typing import List, Dict
 
 
 @dataclass
 class RoomDTO:
+    uuid: str
     project_uuid: str
     external_id: str
     created_on: str
+
+    def to_json(self):
+        return json.dumps(self.__dict__)
--- chats/apps/projects/usecases/send_room_info.py
+++ chats/apps/projects/usecases/send_room_info.py
@@ -8,9 +8,10 @@ class RoomInfoUseCase:
         self._rooms_client = RoomsInfoMixin()
 
     def get_room(self, room: Room):
-        room_dto = {
-            "project_uuid": str(room.project.uuid),
-            "external_id": room.contact.external_id,
-            "created_on": room.created_on.isoformat(),
-        }
-        self._rooms_client.request_room(content=room_dto)
+        room_dto = RoomDTO(
+            uuid=str(room.uuid),
+            project_uuid=str(room.project.uuid),
+            external_id=room.contact.external_id,
+            created_on=room.created_on.isoformat(),
+        )
+        self._rooms_client.request_room(content=room_dto.to_json())
