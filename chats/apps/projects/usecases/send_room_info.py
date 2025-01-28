05810db48776eab5a39995b74878a75eeaa9b040
Eric Costa <ericosta.dev@gmail.com>
2025-01-28 17:06:22 -0300

    refactor: remove RoomDTO and simplify room info serialization

--- chats/apps/projects/usecases/send_room_info.py
+++ chats/apps/projects/usecases/send_room_info.py
@@ -1,5 +1,4 @@
 from chats.apps.api.v1.internal.eda_clients.billing_client import RoomsInfoMixin
-from chats.apps.api.v1.dto.room_dto import RoomDTO
 from chats.apps.rooms.models import Room
 
 
@@ -8,10 +7,10 @@ class RoomInfoUseCase:
         self._rooms_client = RoomsInfoMixin()
 
     def get_room(self, room: Room):
-        room_dto = RoomDTO(
-            uuid=str(room.uuid),
-            project_uuid=str(room.project.uuid),
-            external_id=room.contact.external_id,
-            created_on=room.created_on.isoformat(),
-        )
-        self._rooms_client.request_room(content=room_dto.to_json())
+        room = {
+            "uuid": str(room.uuid),
+            "project_uuid": str(room.project.uuid),
+            "external_id": room.contact.external_id,
+            "created_on": room.created_on.isoformat(),
+        }
+        self._rooms_client.request_room(content=room)
