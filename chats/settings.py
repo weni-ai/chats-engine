add4ceab325ae03f9cc2143abc7b8add291cbba1
Marcello Alexandre <marcello.alexandre@weni.ai>
2025-02-06 11:05:10 -0300

    Test authentication in django_prometheus

--- chats/settings.py
+++ chats/settings.py
@@ -91,6 +91,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "chats.apps.api.v1.prometheus.views.PrometheusAuthenticationMiddleware",
     "django_prometheus.middleware.PrometheusAfterMiddleware",
 ]
 
