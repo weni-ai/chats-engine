bab88708b8da9e293fabf6e1597e99e98d264946
Marcello Alexandre <marcello.alexandre@weni.ai>
2025-02-06 11:07:50 -0300

    Test authentication in django_prometheus

--- chats/apps/api/v1/prometheus/views.py
+++ chats/apps/api/v1/prometheus/views.py
@@ -8,15 +8,15 @@ class PrometheusAuthenticationMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        #if request.path.startswith("/api/prometheus/"):
-        auth_token = request.headers.get("Authorization")
-        prometheus_auth_token = os.environ.get("PROMETHEUS_AUTH_TOKEN")
-        print("prometheus_auth_token", prometheus_auth_token)
-        print("auth_token", auth_token)
+        if request.path.startswith("/api/prometheus/"):
+            auth_token = request.headers.get("Authorization")
+            prometheus_auth_token = os.environ.get("PROMETHEUS_AUTH_TOKEN")
+            print("prometheus_auth_token", prometheus_auth_token)
+            print("auth_token", auth_token)
 
-        expected_token = f"Bearer {prometheus_auth_token}"
-        if not auth_token or auth_token != expected_token:
-            return HttpResponseForbidden("Acesso negado")
+            expected_token = f"Bearer {prometheus_auth_token}"
+            if not auth_token or auth_token != expected_token:
+                return HttpResponseForbidden("Acesso negado")
 
         response = self.get_response(request)
         return response
