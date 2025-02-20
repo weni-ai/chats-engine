89e386c5c9ec71f053961b4b9b87d4076b83c465
AlanJaeger <aland295@gmail.com>
2025-02-20 17:34:24 -0300

    feature: adding custom status to serializer

--- chats/apps/api/v1/internal/dashboard/serializers.py
+++ chats/apps/api/v1/internal/dashboard/serializers.py
@@ -7,6 +7,7 @@ class DashboardAgentsSerializer(serializers.Serializer):
     agent = serializers.SerializerMethodField()
     closed = serializers.IntegerField(allow_null=True, required=False)
     status = serializers.SerializerMethodField()
+    custom_status = serializers.SerializerMethodField()
 
     def get_link(self, obj):
         return {
@@ -21,3 +22,27 @@ class DashboardAgentsSerializer(serializers.Serializer):
 
     def get_agent(self, obj):
         return f"{obj.get('first_name')} {obj.get('last_name')}"
+
+    def get_custom_status(self, obj):
+        custom_status_list = obj.get("custom_status")
+
+        if not custom_status_list:
+            return None
+
+        aggregated_status = {}
+
+        for status_item in custom_status_list:
+            status_type = status_item.get("status_type")
+            break_time = status_item.get("break_time", 0)
+
+            if status_type in aggregated_status:
+                aggregated_status[status_type] += break_time
+            else:
+                aggregated_status[status_type] = break_time
+
+        aggregated_status_list = [
+            {"status_type": status_type, "break_time": total_time}
+            for status_type, total_time in aggregated_status.items()
+        ]
+
+        return aggregated_status_list
