be2b70c2d286720016d2a5a75696cdf4f2f6107d
AlanJaeger <aland295@gmail.com>
2025-02-26 18:14:07 -0300

    feature: new serializer for custom breaks

--- chats/apps/api/v1/internal/dashboard/serializers.py
+++ chats/apps/api/v1/internal/dashboard/serializers.py
@@ -7,6 +7,28 @@ class DashboardAgentsSerializer(serializers.Serializer):
     agent = serializers.SerializerMethodField()
     closed = serializers.IntegerField(allow_null=True, required=False)
     status = serializers.SerializerMethodField()
+
+    def get_link(self, obj):
+        return {
+            "url": f"chats:dashboard/view-mode/{obj.get('email', '')}",
+            "type": "internal",
+        }
+
+    def get_status(self, obj):
+        if obj.get("status") == "ONLINE":
+            return "green"
+        return "gray"
+
+    def get_agent(self, obj):
+        return f"{obj.get('first_name')} {obj.get('last_name')}"
+
+
+class DashboardCustomAgentStatusSerializer(serializers.Serializer):
+    link = serializers.SerializerMethodField()
+    opened = serializers.IntegerField(allow_null=True, required=False)
+    agent = serializers.SerializerMethodField()
+    closed = serializers.IntegerField(allow_null=True, required=False)
+    status = serializers.SerializerMethodField()
     custom_status = serializers.SerializerMethodField()
 
     def get_link(self, obj):
