dc4406e92ccc4af3dc365d1631feb5854de0739c
AlanJaeger <aland295@gmail.com>
2025-02-26 16:15:27 -0300

    feature: usign get to get messages

--- chats/apps/projects/usecases/update_status.py
+++ chats/apps/projects/usecases/update_status.py
@@ -1,11 +1,20 @@
 from chats.apps.msgs.models import Message, MessageMedia
+from django.db import transaction
 
 
 class UpdateStatusMessageUseCase:
 
     def update_status_message(self, external_id, message_status):
-        message = Message.objects.filter(external_id=external_id)
-        if not message:
-            message = MessageMedia.objects.filter(external_id=external_id)
-        message.status = message_status
-        message.save()
+        with transaction.atomic():
+            try:
+                message = Message.objects.select_for_update().get(external_id=external_id)
+                message.status = message_status
+                message.save(update_fields=['status'])
+                return
+            except Message.DoesNotExist:
+                try:
+                    message_media = MessageMedia.objects.select_for_update().get(external_id=external_id)
+                    message_media.message.status = message_status
+                    message_media.message.save(update_fields=['status'])
+                except MessageMedia.DoesNotExist:
+                    return
