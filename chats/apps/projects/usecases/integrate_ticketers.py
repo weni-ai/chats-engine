67fd0d6957615c1f52821b61460db804e056a205
Eric Costa <ericosta.dev@gmail.com>
2025-02-26 16:43:32 -0300

    feat: Modify queue integration to support multiple queues per sector

--- chats/apps/projects/usecases/integrate_ticketers.py
+++ chats/apps/projects/usecases/integrate_ticketers.py
@@ -1,12 +1,12 @@
-from chats.apps.projects.models import Project
-from chats.apps.sectors.models import Sector
-from chats.apps.queues.models import Queue
+from rest_framework import exceptions, status
 
 from chats.apps.api.v1.internal.rest_clients.connect_rest_client import (
     ConnectRESTClient,
 )
-from rest_framework import exceptions, status
 from chats.apps.api.v1.internal.rest_clients.flows_rest_client import FlowRESTClient
+from chats.apps.projects.models import Project
+from chats.apps.queues.models import Queue
+from chats.apps.sectors.models import Sector
 
 
 class IntegratedTicketers:
@@ -108,27 +108,29 @@ class IntegratedTicketers:
 
     def integrate__individual_topic(self, project, sector_integrated_token):
         try:
-            queue = Queue.objects.filter(
+            queues = Queue.objects.filter(
                 sector__project=project,
                 sector__config__secondary_project=str(sector_integrated_token),
             )
-            content = {
-                "uuid": str(queue.uuid),
-                "name": queue.name,
-                "sector_uuid": str(queue.sector.uuid),
-                "project_uuid": str(queue.sector.config.get("secondary_project")),
-            }
-            response = FlowRESTClient().create_queue(**content)
-            if response.status_code not in [
-                status.HTTP_200_OK,
-                status.HTTP_201_CREATED,
-            ]:
-                raise exceptions.APIException(
-                    detail=(
-                        f"[{response.status_code}] Error posting the queue on flows. "
-                        f"Exception: {response.content}"
+
+            for queue in queues:
+                content = {
+                    "uuid": str(queue.uuid),
+                    "name": queue.name,
+                    "sector_uuid": str(queue.sector.uuid),
+                    "project_uuid": str(queue.sector.config.get("secondary_project")),
+                }
+                response = FlowRESTClient().create_queue(**content)
+                if response.status_code not in [
+                    status.HTTP_200_OK,
+                    status.HTTP_201_CREATED,
+                ]:
+                    raise exceptions.APIException(
+                        detail=(
+                            f"[{response.status_code}] Error posting the queue on flows. "
+                            f"Exception: {response.content}"
+                        )
                     )
-                )
         except Exception as e:
             raise exceptions.APIException(
                 detail=f"There is no secondary project for that queue. Error: {e}"
