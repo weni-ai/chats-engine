3640b27e4c2d7edbdd14e2570cf908c2f401a47a
Eric Costa <ericosta.dev@gmail.com>
2025-04-03 16:15:17 -0300

    removing old itens

--- chats/apps/projects/handle.py
+++ chats/apps/projects/handle.py
@@ -6,8 +6,6 @@ from .consumers import (
     DeadLetterConsumer,
     ProjectPermissionConsumer,
     SectorConsumer,
-    MessageConsumer,
-    MessageStatusConsumer,
 )
 
 
@@ -19,7 +17,5 @@ def handle_consumers(channel: Channel) -> None:
     channel.basic_consume(
         "chats.permissions", callback=ProjectPermissionConsumer().handle
     )
-    channel.basic_consume("flows.msgs", callback=MessageConsumer().handle)
-    channel.basic_consume("billing.msgs", callback=MessageStatusConsumer().handle)
     channel.basic_consume("chats.integrated-feature", callback=SectorConsumer().handle)
     channel.basic_consume("chats.dlq", callback=DeadLetterConsumer.consume)
