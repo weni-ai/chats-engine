348cd92d15b7b57cc6f14571b375f737f7727e9e
AlanJaeger <aland295@gmail.com>
2025-04-07 15:16:16 -0300

    feature: history endpoint and process message history in serializer

--- chats/apps/api/v1/external/rooms/serializers.py
+++ chats/apps/api/v1/external/rooms/serializers.py
@@ -1,6 +1,6 @@
-import pendulum
 from typing import Dict, List
 
+import pendulum
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
@@ -13,6 +13,7 @@ from chats.apps.api.v1.queues.serializers import QueueSerializer
 from chats.apps.api.v1.sectors.serializers import TagSimpleSerializer
 from chats.apps.contacts.models import Contact
 from chats.apps.dashboard.models import RoomMetrics
+from chats.apps.msgs.models import Message, MessageMedia
 from chats.apps.queues.models import Queue
 from chats.apps.rooms.models import Room
 from chats.apps.rooms.views import close_room
@@ -158,6 +159,11 @@ class RoomMetricsSerializer(serializers.ModelSerializer):
         return None
 
 
+class ProjectInfoSerializer(serializers.Serializer):
+    uuid = serializers.UUIDField(required=False, read_only=False)
+    name = serializers.CharField(required=False, read_only=False)
+
+
 class RoomFlowSerializer(serializers.ModelSerializer):
     user = UserSerializer(many=False, required=False, read_only=True)
     user_email = serializers.SlugRelatedField(
@@ -179,6 +185,10 @@ class RoomFlowSerializer(serializers.ModelSerializer):
     flow_uuid = serializers.CharField(required=False, write_only=True, allow_null=True)
     is_anon = serializers.BooleanField(write_only=True, required=False, default=False)
     ticket_uuid = serializers.UUIDField(required=False)
+    history = serializers.ListField(
+        child=serializers.DictField(), required=False, write_only=True
+    )
+    project_info = ProjectInfoSerializer(required=False, write_only=True)
 
     class Meta:
         model = Room
@@ -203,6 +213,8 @@ class RoomFlowSerializer(serializers.ModelSerializer):
             "urn",
             "is_anon",
             "protocol",
+            "history",
+            "project_info",
         ]
         read_only_fields = [
             "uuid",
@@ -215,6 +227,8 @@ class RoomFlowSerializer(serializers.ModelSerializer):
         extra_kwargs = {"queue": {"required": False, "read_only": True}}
 
     def create(self, validated_data):
+        history_data = validated_data.pop("history", [])
+
         queue, sector = self.get_queue_and_sector(validated_data)
         project = sector.project
 
@@ -235,6 +249,8 @@ class RoomFlowSerializer(serializers.ModelSerializer):
         room = get_active_room_flow_start(contact, flow_uuid, project)
 
         if room is not None:
+            if history_data:
+                self.process_message_history(room, history_data)
             return room
 
         room = self.validate_unique_active_project(contact, project)
@@ -256,6 +272,9 @@ class RoomFlowSerializer(serializers.ModelSerializer):
             service_chat=service_chat,
         )
         RoomMetrics.objects.create(room=room)
+
+        if history_data:
+            self.process_message_history(room, history_data)
 
         return room
 
@@ -324,3 +343,67 @@ class RoomFlowSerializer(serializers.ModelSerializer):
         return Contact.objects.update_or_create(
             external_id=contact_external_id, defaults=contact_data
         )
+
+    def process_message_history(self, room, messages_data):
+        is_waiting = room.get_is_waiting()
+        was_24h_valid = room.is_24h_valid
+        need_update_room = False
+        any_incoming_msgs = False
+
+        messages_to_create = []
+        media_data_map = {}
+
+        for message_index, msg_data in enumerate(messages_data):
+            direction = msg_data.pop("direction")
+            medias = msg_data.pop("attachments", [])
+            text = msg_data.get("text")
+            created_on = msg_data.get("created_on")
+
+            if text is None and not medias:
+                raise serializers.ValidationError(
+                    {"detail": "Cannot create message without text or media"}
+                )
+
+            if direction == "incoming":
+                msg_data["contact"] = room.contact
+                any_incoming_msgs = True
+
+                if is_waiting:
+                    need_update_room = True
+                    room.is_waiting = False
+                elif not was_24h_valid:
+                    need_update_room = True
+
+            msg_data["room"] = room
+            msg_data["created_on"] = created_on
+            message = Message(**msg_data)
+            messages_to_create.append(message)
+
+            if medias:
+                media_data_map[message_index] = medias
+
+        if need_update_room:
+            room.save()
+
+        if messages_to_create:
+            created_messages = Message.objects.bulk_create(messages_to_create)
+
+            all_media = []
+            for message_index, message in enumerate(created_messages):
+                if message_index in media_data_map:
+                    for media_data in media_data_map[message_index]:
+                        all_media.append(
+                            MessageMedia(
+                                content_type=media_data["content_type"],
+                                media_url=media_data["url"],
+                                message=message,
+                            )
+                        )
+
+            if all_media:
+                MessageMedia.objects.bulk_create(all_media)
+
+            room.notify_room("create")
+
+            if room.user is None and room.contact and any_incoming_msgs:
+                room.trigger_default_message()
