466eb011d0a4217cf9c32fda8f1d116b56d150e9
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2025-04-24 15:49:44 -0300

    feat: implement queue priority routing for project permissions
    - Added a call to start queue priority routing for all queues in a project when a user's status changes to online in the ProjectPermissionViewset.
    - Introduced a new utility function to handle the routing logic for all queues associated with a project, ensuring proper configuration checks and logging for better traceability.

--- chats/apps/queues/utils.py
+++ chats/apps/queues/utils.py
@@ -1,5 +1,6 @@
 import logging
 from django.conf import settings
+from chats.apps.projects.models.models import Project
 from chats.apps.queues.models import Queue
 from chats.apps.queues.tasks import route_queue_rooms
 
@@ -29,3 +30,25 @@ def start_queue_priority_routing(queue: Queue):
 
     logger.info("Calling route_queue_rooms for queue %s asynchronously", queue.uuid)
     route_queue_rooms.delay(queue.uuid)
+
+
+def start_queue_priority_routing_for_all_queues_in_project(project: Project):
+    """
+    Start routing rooms for all queues in a project, if the project is configured to use priority routing.
+    """
+    if not project.use_queue_priority_routing:
+        logger.info(
+            "Skipping start_queue_priority_routing_for_all_queues_in_project for project %s because it is not configured to use priority routing",
+            project.uuid,
+        )
+        return
+
+    queues = Queue.objects.filter(sector__project=project)
+
+    logger.info(
+        "Started routing rooms for all queues in project %s",
+        project.uuid,
+    )
+
+    for queue in queues:
+        start_queue_priority_routing(queue)
