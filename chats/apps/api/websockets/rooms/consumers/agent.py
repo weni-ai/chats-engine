526c1bac6559db1ef95924274d830a52d354270d
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2025-04-28 15:40:16 -0300

    feat: enhance connection check logic to validate user email

--- chats/apps/api/websockets/rooms/consumers/agent.py
+++ chats/apps/api/websockets/rooms/consumers/agent.py
@@ -209,7 +209,10 @@ class AgentRoomConsumer(AsyncJsonWebsocketConsumer):
                 event["content"].get("connection_id"),
                 event["content"].get("user_email"),
             )
-            if event["content"].get("connection_id") != str(self.connection_id):
+            if (
+                event["content"].get("connection_id") != str(self.connection_id)
+                and event["content"].get("user_email") == self.user.email
+            ):
                 logger.info(
                     "Connection ID: %s sending connection check response to user %s",
                     self.connection_id,
