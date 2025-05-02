23b19aa00187e58918156add81ae099764b53bba
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2025-05-02 08:47:24 -0300

    Update app name for AiFeaturesConfig to reflect new module structure

--- chats/apps/ai_features/apps.py
+++ chats/apps/ai_features/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AiFeaturesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "ai_features"
+    name = "chats.apps.ai_features"
