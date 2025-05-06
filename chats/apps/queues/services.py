3bc81e2b566ebf5bbbbf4c0da0696ecb0532e5fd
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2025-05-06 08:32:22 -0300

    fix: update notification method in QueueRouterService
    - Changed the notification method from room.notify_room("update") to room.notify_queue("update") to correctly notify the queue about updates, improving the accuracy of real-time communication.

--- chats/apps/queues/services.py
+++ chats/apps/queues/services.py
@@ -72,7 +72,7 @@ class QueueRouterService:
             room.save()
 
             room.notify_user("update")
-            room.notify_room("update")
+            room.notify_queue("update")
 
             feedback = create_transfer_json(
                 action="auto_assign_from_queue",
