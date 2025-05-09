ba73f8a44e04d44907ed77eb88bf4766a60a7b98
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2025-05-09 14:31:17 -0300

    Refactor AI Features authentication and remove obsolete tests
    - Removed the `validate` method from `FeaturePromptWriteSerializer` to simplify validation logic.
    - Refactored `AIFeaturesAuthentication` to extend `BaseHMACSignatureAuthentication`, streamlining the authentication process.
    - Deleted outdated test files for authentication, as they are no longer relevant to the current implementation.
    - Added new tests for handling invalid signatures in feature prompt retrieval and creation.

--- chats/apps/api/v1/internal/ai_features/serializers.py
+++ chats/apps/api/v1/internal/ai_features/serializers.py
@@ -1,4 +1,3 @@
-from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 
 from chats.apps.ai_features.models import FeaturePrompt
@@ -8,18 +7,6 @@ class FeaturePromptWriteSerializer(serializers.ModelSerializer):
     class Meta:
         model = FeaturePrompt
         fields = ["uuid", "feature", "model", "settings", "prompt", "version"]
-
-    def validate(self, attrs: dict) -> dict:
-        feature = attrs.get("feature")
-        version = attrs.get("version")
-
-        if FeaturePrompt.objects.filter(feature=feature, version=version).exists():
-            raise serializers.ValidationError(
-                {"version": [_("Feature prompt with this version already exists")]},
-                code="feature_prompt_version_already_exists",
-            )
-
-        return attrs
 
 
 class FeaturePromptReadSerializer(serializers.ModelSerializer):
--- chats/apps/api/v1/internal/ai_features/tests/test_views.py
+++ chats/apps/api/v1/internal/ai_features/tests/test_views.py
@@ -55,6 +55,21 @@ class FeaturePromptsViewTests(BaseFeaturePromptsViewTests):
         self.assertEqual(len(response.data), 1)
         self.assertEqual(response.data[0]["feature"], "example")
 
+    @override_settings(AI_FEATURES_PROMPTS_API_SECRET="test_secret")
+    def test_get_feature_prompts_with_invalid_signature(self):
+        timestamp = str(int(time.time()))
+        signature = hmac.new(
+            bytes("test_secret", "utf-8"), bytes("invalid", "utf-8"), "sha256"
+        ).hexdigest()
+
+        self.client.credentials(
+            HTTP_X_WENI_SIGNATURE=signature, HTTP_X_TIMESTAMP=timestamp
+        )
+
+        response = self.get_feature_prompts()
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
     def test_create_feature_prompt_without_authentication(self):
         response = self.create_feature_prompt(
             {
@@ -94,3 +109,54 @@ class FeaturePromptsViewTests(BaseFeaturePromptsViewTests):
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data["feature"], "example")
+
+    @override_settings(AI_FEATURES_PROMPTS_API_SECRET="test_secret")
+    def test_create_feature_prompt_with_invalid_signature(self):
+        body = {
+            "feature": "example",
+            "model": "example",
+            "settings": {"test": "test"},
+            "prompt": "Test Prompt",
+            "version": 2,
+        }
+
+        timestamp = str(int(time.time()))
+
+        signature = hmac.new(
+            bytes("test_secret", "utf-8"), bytes("invalid", "utf-8"), "sha256"
+        ).hexdigest()
+
+        self.client.credentials(
+            HTTP_X_WENI_SIGNATURE=signature, HTTP_X_TIMESTAMP=timestamp
+        )
+
+        response = self.create_feature_prompt(body)
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    @override_settings(AI_FEATURES_PROMPTS_API_SECRET="test_secret")
+    def test_create_feature_prompt_with_existing_version(self):
+        body = {
+            "feature": "example",
+            "model": "example",
+            "settings": {"test": "test"},
+            "prompt": "Test Prompt",
+            "version": 1,
+        }
+
+        timestamp = str(int(time.time()))
+
+        message = json.dumps(body, separators=(",", ":")) + timestamp
+
+        signature = hmac.new(
+            bytes("test_secret", "utf-8"), bytes(message, "utf-8"), "sha256"
+        ).hexdigest()
+
+        self.client.credentials(
+            HTTP_X_WENI_SIGNATURE=signature, HTTP_X_TIMESTAMP=timestamp
+        )
+
+        response = self.create_feature_prompt(body)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data["non_field_errors"][0].code, "unique")
