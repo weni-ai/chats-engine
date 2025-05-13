6136ef4716c5e98a58229426dd4001092fa3af19
AlanJaeger <aland295@gmail.com>
2025-05-13 17:07:30 -0300

    feature: some optimizations for history

--- chats/apps/history/filters/rooms_filter.py
+++ chats/apps/history/filters/rooms_filter.py
@@ -13,9 +13,8 @@ class HistoryRoomFilter(filters.FilterSet):
         fields = []
 
     contact = filters.CharFilter(
-        field_name="contact",
+        field_name="contact__external_id",
         required=False,
-        method="filter_contact",
         help_text=_("Contact's External ID"),
     )
 
@@ -27,9 +26,8 @@ class HistoryRoomFilter(filters.FilterSet):
     )
 
     sector = filters.CharFilter(
-        field_name="sector",
+        field_name="queue__sector__uuid",
         required=False,
-        method="filter_sector",
         help_text=_("Sector's UUID"),
     )
 
@@ -41,53 +39,57 @@ class HistoryRoomFilter(filters.FilterSet):
     created_on = filters.DateFromToRangeFilter(
         field_name="created_on",
         required=False,
+        lookup_expr="date",
         help_text=_("Room created on"),
     )
     ended_at = filters.DateFromToRangeFilter(
         field_name="ended_at",
         required=False,
+        lookup_expr="date",
         help_text=_("Room ended at"),
     )
 
     def filter_project(self, queryset, name, value):
-        qs = queryset
         user = self.request.user
+
         try:
-            user_permission = user.project_permissions.get(project=value)
+            user_permission = user.project_permissions.select_related("project").get(
+                project=value
+            )
         except ObjectDoesNotExist:
             raise exceptions.APIException(
                 detail="Access denied! Make sure you have the right permission to access this project"
             )
 
-        contacts_blocklist = user_permission.project.history_contacts_blocklist
-        if contacts_blocklist:
-            qs = qs.exclude(contact__external_id__in=contacts_blocklist)
+        base_queryset = queryset.filter(is_active=False, ended_at__isnull=False)
 
         project = user_permission.project
+        contacts_blocklist = project.history_contacts_blocklist
+        if contacts_blocklist:
+            base_queryset = base_queryset.exclude(
+                contact__external_id__in=contacts_blocklist
+            )
+
         if (
             user_permission.is_admin is False
             and project.agents_can_see_queue_history is False
         ):
-            return qs.filter(
-                user=user,
-                queue__sector__project=value,
-                is_active=False,
-                ended_at__isnull=False,
-            )
-        queue_ids = user_permission.queue_ids
+            return base_queryset.filter(user=user, queue__sector__project=value)
 
-        return qs.filter(
-            Q(queue__in=queue_ids) | Q(user=user, queue__sector__project=value),
-            is_active=False,
-            ended_at__isnull=False,
+        queue_ids = user_permission.queue_ids
+        return base_queryset.filter(
+            Q(queue__in=queue_ids) | Q(user=user, queue__sector__project=value)
         )
 
     def filter_sector(self, queryset, name, value):
-        return queryset.filter(queue__sector__uuid=value)
+        return queryset
 
     def filter_tags(self, queryset, name, value):
+        if not value:
+            return queryset
+
         values = value.split(",")
-        return queryset.filter(tags__name__in=values)
+        return queryset.filter(tags__name__in=values).distinct()
 
     def filter_contact(self, queryset, name, value):
-        return queryset.filter(contact__external_id=value)
+        return queryset
