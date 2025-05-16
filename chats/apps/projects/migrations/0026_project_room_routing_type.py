bc25b3bd8ebaeb74a795b9e9858fdc6bb38adbdc
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2025-05-16 15:40:36 -0300

    chore: remove obsolete migration and add new field for project routing type
    - Deleted the migration file for the custom status type configuration.
    - Introduced a new migration to add the `room_routing_type` field to the project model, allowing for routing options between general and queue priority.

--- chats/apps/projects/migrations/0026_project_room_routing_type.py
+++ chats/apps/projects/migrations/0026_project_room_routing_type.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("projects", "0026_customstatustype_config"),
+        ("projects", "0025_alter_customstatustype_unique_together"),
     ]
 
     operations = [
