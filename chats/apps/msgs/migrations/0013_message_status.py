eaeb702755f0837bc16e9b6b9d3f36b7bf501500
AlanJaeger <aland295@gmail.com>
2025-05-21 15:59:18 -0300

    feature: resolving migration conflict

--- chats/apps/msgs/migrations/0013_message_status.py
+++ chats/apps/msgs/migrations/0013_message_status.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("msgs", "0011_alter_message_metadata"),
+        ("msgs", "0012_chatmessagereplyindex"),
     ]
 
     operations = [
