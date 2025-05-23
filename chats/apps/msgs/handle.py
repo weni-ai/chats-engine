e9ccbe88e17ff5e126e880c75257b438ba265e29
AlanJaeger <aland295@gmail.com>
2025-05-23 16:10:44 -0300

    feature: adding queue config to rmq

--- chats/apps/msgs/handle.py
+++ chats/apps/msgs/handle.py
@@ -1,7 +1,8 @@
 from amqp.channel import Channel
 
-from chats.apps.msgs.consumers import MsgConsumer
+from chats.apps.msgs.consumers import MsgConsumer, MessageStatusConsumer
 
 
 def handle_consumers(channel: Channel) -> None:
     channel.basic_consume("chats.msgs", callback=MsgConsumer().handle)
+    channel.basic_consume("chats.msgs-status", callback=MessageStatusConsumer().handle)
