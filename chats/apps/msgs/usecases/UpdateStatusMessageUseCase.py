b5e3c5d4c5a2a005c99560d4c92bbe66aa845527
AlanJaeger <aland295@gmail.com>
2025-06-12 19:01:37 -0300

    feature: adding flag in settings

--- chats/apps/msgs/usecases/UpdateStatusMessageUseCase.py
+++ chats/apps/msgs/usecases/UpdateStatusMessageUseCase.py
@@ -1,6 +1,7 @@
+from django.conf import settings
 from django.db import models
 
-from chats.apps.msgs.models import Message, MessageMedia
+from chats.apps.msgs.models import ChatMessageReplyIndex
 from chats.utils.websockets import send_channels_group
 
 
@@ -15,65 +16,36 @@ class MessageStatusNotifier:
         )
 
     @classmethod
-    def find_and_notify_for_message(cls, message_id, message_status):
-        message_data = (
-            Message.objects.filter(
-                external_id=message_id, room__is_active=True, room__user__isnull=False
-            )
-            .values_list("uuid", "room__user__project_permissions__pk")
-            .filter(
-                room__user__project_permissions__project=models.F(
-                    "room__queue__sector__project"
-                )
-            )
-            .first()
-        )
-
-        if message_data:
-            uuid, permission_pk = message_data
-            cls.notify_status_update(uuid, message_status, permission_pk)
-            return True
-        return False
-
-    @classmethod
-    def find_and_notify_for_media(cls, message_id, message_status):
-        media_data = (
-            MessageMedia.objects.filter(
-                external_id=message_id,
-                message__room__is_active=True,
-                message__room__user__isnull=False,
-            )
-            .values_list(
-                "message__uuid", "message__room__user__project_permissions__pk"
-            )
-            .filter(
-                message__room__user__project_permissions__project=models.F(
-                    "message__room__queue__sector__project"
-                )
-            )
-            .first()
-        )
-
-        if media_data:
-            uuid, permission_pk = media_data
-            cls.notify_status_update(uuid, message_status, permission_pk)
-            return True
+    def notify_for_message(cls, message, message_status):
+        if message and message.room and message.room.user:
+            project = message.room.project
+            if project:
+                permission = message.room.user.project_permissions.filter(
+                    project=project
+                ).first()
+                if permission:
+                    cls.notify_status_update(
+                        message.uuid, message_status, permission.pk
+                    )
+                    return True
         return False
 
 
 class UpdateStatusMessageUseCase:
     def update_status_message(self, message_id, message_status):
-        rows_updated = Message.objects.filter(external_id=message_id).update(
-            status=message_status
-        )
-        if rows_updated > 0:
-            MessageStatusNotifier.find_and_notify_for_message(
-                message_id, message_status
-            )
-            return
+        try:
+            reply_index = ChatMessageReplyIndex.objects.get(external_id=message_id)
+            message = reply_index.message
 
-        media_rows_updated = MessageMedia.objects.filter(external_id=message_id).update(
-            message__status=message_status
-        )
-        if media_rows_updated > 0:
-            MessageStatusNotifier.find_and_notify_for_media(message_id, message_status)
+            project_uuid = str(message.room.project.uuid)
+            if project_uuid not in settings.MESSAGE_STATUS_UPDATE_ENABLED_PROJECTS:
+                return
+
+            message.status = message_status
+            message.save(update_fields=["status"])
+            MessageStatusNotifier.notify_for_message(message, message_status)
+
+        except ChatMessageReplyIndex.DoesNotExist:
+            print(
+                f"Message with external_id {message_id} not found in ChatMessageReplyIndex"
+            )
