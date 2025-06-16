04f2e44ff501c7f41e9de2d791e905e369bd16cb
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2025-06-16 16:12:56 -0300

    refactor: update generate_text method signature and request body formatting
    - Changed the `generate_text` method in the `BaseAIPlatformClient` to accept `prompt_settings` and `prompt` as parameters instead of a single `request_body` dictionary.
    - Introduced a new `format_request_body` method in the `BedrockClient` to handle the construction of the request body based on the model type, improving code organization and clarity.
    - Updated the `HistorySummaryService` to utilize the new method signature, enhancing the integration with the AI model platform.

--- chats/apps/ai_features/integrations/aws/bedrock/client.py
+++ chats/apps/ai_features/integrations/aws/bedrock/client.py
@@ -51,11 +51,46 @@ class BedrockClient(BaseAIPlatformClient):
         # If neither format matches, raise an error
         raise ValueError(f"Unsupported response format: {response_body}")
 
-    def generate_text(self, request_body: dict) -> str:
+    def format_request_body(self, prompt_settings: dict, prompt: str) -> dict:
+        """
+        Format the request body for the Bedrock client.
+        """
+        settings_body = {}
+
+        for setting, value in prompt_settings.items():
+            settings_body[setting] = value
+
+        if "anthropic.claude" in self.model_id:
+            return {
+                "messages": [
+                    {
+                        "role": "user",
+                        "content": [{"type": "text", "text": prompt}],
+                    }
+                ],
+                **settings_body,
+            }
+
+        if "amazon.nova" in self.model_id:
+            return {
+                "messages": [
+                    {
+                        "role": "user",
+                        "content": [{"text": prompt}],
+                    }
+                ],
+                **settings_body,
+            }
+
+        raise ValueError(f"Unsupported model: {self.model_id}")
+
+    def generate_text(self, prompt_settings: dict, prompt: str) -> str:
         """
         Generate text using the Bedrock client.
         """
         try:
+            request_body = self.format_request_body(prompt_settings, prompt)
+
             response = self.client.invoke_model(
                 modelId=self.model_id,
                 body=json.dumps(request_body),
