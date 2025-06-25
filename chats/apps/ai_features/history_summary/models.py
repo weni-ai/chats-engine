f3562d9c5029b031d7ae56543cd0a0d25322e7af
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2025-06-25 09:46:14 -0300

    feat: add HistorySummaryFeedback model for user feedback on summaries
    - Introduced a new model `HistorySummaryFeedback` to capture user feedback on history summaries, including fields for user, feedback text, and a liked status.
    - Added a unique constraint to ensure a user can only provide one feedback entry per history summary.
    - Created a migration to implement the new model in the database.

--- chats/apps/ai_features/history_summary/models.py
+++ chats/apps/ai_features/history_summary/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+from chats.apps.accounts.models import User
 from chats.apps.ai_features.models import FeaturePrompt
 from chats.apps.rooms.models import Room
 from chats.core.models import BaseModel
@@ -43,3 +44,37 @@ class HistorySummary(BaseModel):
     def update_status(self, status: HistorySummaryStatus):
         self.status = status
         self.save(update_fields=["status"])
+
+
+class HistorySummaryFeedback(BaseModel):
+    """
+    A model to store the user feedback of the history summary.
+    """
+
+    history_summary = models.ForeignKey(
+        HistorySummary,
+        verbose_name=_("History Summary"),
+        on_delete=models.CASCADE,
+        related_name="feedbacks",
+    )
+    user = models.ForeignKey(
+        User,
+        verbose_name=_("User"),
+        on_delete=models.CASCADE,
+        related_name="history_summary_feedbacks",
+    )
+    liked = models.BooleanField(_("Liked?"))
+    feedback = models.CharField(_("Feedback"), max_length=150, blank=True, null=True)
+
+    class Meta:
+        verbose_name = _("History Summary Feedback")
+        verbose_name_plural = _("History Summary Feedbacks")
+        constraints = [
+            models.UniqueConstraint(
+                fields=["history_summary", "user"],
+                name="unique_history_summary_user_feedback",
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.history_summary.pk} - {self.user.email}"
