a95d8cd5e838dc53c3c7f9857b88344bda96b009
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2025-06-26 15:11:25 -0300

    fix: ensure room is saved after notifying user of updates
    - Added a call to `room.save()` in the RoomUserExternalViewSet to persist changes after adding transfer feedback.
    - This change ensures that the room's state is consistently updated before notifying the user, preventing potential data inconsistencies.

--- chats/apps/api/v1/external/rooms/viewsets.py
+++ chats/apps/api/v1/external/rooms/viewsets.py
@@ -269,6 +269,7 @@ class RoomUserExternalViewSet(viewsets.ViewSet):
             from_="",
             to=room.user,
         )
+        room.save()
         room.add_transfer_to_history(feedback)
 
         room.notify_user("update", user=None)
