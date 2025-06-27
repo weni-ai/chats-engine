b241c728c6a3a1252aea1ff1a5db215acb7e21a3
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2025-06-27 08:03:38 -0300

    feat: add migration for added_to_queue_at field in Room model
    - Created a new migration to add the `added_to_queue_at` DateTimeField to the Room model, allowing tracking of when a room is added to the queue.

--- chats/apps/rooms/migrations/0019_room_added_to_queue_at.py
+++ chats/apps/rooms/migrations/0019_room_added_to_queue_at.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("rooms", "0017_roompin_roompin_unique_room_user_room_pin"),
+        ("rooms", "0018_room_full_transfer_history"),
     ]
 
     operations = [
