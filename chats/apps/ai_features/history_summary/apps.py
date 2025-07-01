8a7ce542b6c3f2b0d056f6bbe36f48aa5e934ee4
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2025-07-01 14:24:00 -0300

    refactor: rename AiFeaturesConfig to HistorySummaryConfig for clarity
    - Updated the application configuration class name to better reflect its purpose within the history summary feature.

--- chats/apps/ai_features/history_summary/apps.py
+++ chats/apps/ai_features/history_summary/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AiFeaturesConfig(AppConfig):
+class HistorySummaryConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "chats.apps.ai_features.history_summary"
