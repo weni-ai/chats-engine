96674b405f1e74229e474d44e55e0ee9a6b758c0
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2025-07-30 17:52:47 -0300

    Add task to update Growthbook feature flags definitions
    - Implemented a new Celery task to update feature flags definitions using the Growthbook client.
    - This task enhances the automation of feature flag management within the application.

---
+++ chats/apps/feature_flags/integrations/growthbook/tasks.py
@@ -0,0 +1,10 @@
+from chats.celery import app
+from chats.apps.feature_flags.integrations.growthbook.singleton import GROWTHBOOK_CLIENT
+
+
+@app.task
+def update_growthbook_feature_flags():
+    """
+    Update the growthbook feature flags definitions.
+    """
+    GROWTHBOOK_CLIENT.update_feature_flags_definitions()
