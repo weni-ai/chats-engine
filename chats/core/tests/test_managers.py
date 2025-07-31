72857924e549fb7215745311b3d4f642138cf89a
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2025-07-31 15:27:20 -0300

    Remove outdated test files for Queue and Sector models; add new tests for SoftDeletableManager functionality in core module.

---
+++ chats/core/tests/test_managers.py
@@ -0,0 +1,35 @@
+from django.test import TestCase
+from django.db import models
+
+from chats.core.managers import SoftDeletableManager
+from chats.core.models import BaseSoftDeleteModel
+
+
+class TestOnlyModel(BaseSoftDeleteModel):
+    """
+    A test-only model that won't exist outside these tests.
+    This model is used to test the SoftDeletableManager functionality.
+    """
+
+    name = models.CharField(max_length=100)
+
+    objects = SoftDeletableManager()
+    all_objects = SoftDeletableManager(include_deleted=True)
+
+    class Meta:
+        app_label = "core"
+        db_table = "test_only_soft_deletable_model"
+
+
+class SoftDeletableManagerTests(TestCase):
+    def setUp(self):
+        self.instance = TestOnlyModel.objects.create(name="Example")
+
+    def test_get_queryset_with_include_deleted(self):
+        self.assertIn(self.instance, TestOnlyModel.objects.all())
+        self.assertIn(self.instance, TestOnlyModel.all_objects.all())
+
+        self.instance.delete()
+
+        self.assertNotIn(self.instance, TestOnlyModel.objects.all())
+        self.assertIn(self.instance, TestOnlyModel.all_objects.all())
