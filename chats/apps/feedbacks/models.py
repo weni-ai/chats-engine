b9f17ab3aeefa8bb04b1802d8ed68fdfc19444a1
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2025-08-21 14:09:52 -0300

    feat: implement user feedback model and choices
    - Added a new `FeedbackRate` class for defining feedback rating choices.
    - Created `UserFeedback` model to store user feedback with fields for user, rate, text, and associated project.
    - Generated initial migration for the `UserFeedback` model to establish database schema.

--- chats/apps/feedbacks/models.py
+++ chats/apps/feedbacks/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+from chats.core.models import BaseModel
+from chats.apps.feedbacks.choices import FeedbackRate
+
+
+class UserFeedback(BaseModel):
+    """
+    User feedback model
+    """
+
+    user = models.ForeignKey(
+        "accounts.User",
+        on_delete=models.CASCADE,
+        related_name="user_feedbacks",
+    )
+    rate = models.IntegerField(choices=FeedbackRate.choices)
+    text = models.TextField(null=True, blank=True)
+    project = models.ForeignKey(
+        "projects.Project",
+        related_name="user_feedbacks",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+
+    class Meta:
+        verbose_name = "User Feedback"
+        verbose_name_plural = "User Feedbacks"
+
+    def __str__(self):
+        return f"Feedback from {self.user.email} {self.rate}"
