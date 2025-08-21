7e53445b81441b871cccbc3f5c7f6fbcee75370a
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2025-08-21 11:26:10 -0300

    refactor: consolidate ASGI configuration into a single file
    - Removed the old ASGI files for HTTP and WebSocket handling.
    - Introduced a new unified ASGI configuration in `asgi.py` to streamline the application setup.
    - The new configuration includes routing for both HTTP and WebSocket protocols.

---
+++ chats/asgi.py
@@ -0,0 +1,28 @@
+"""
+ASGI config for chats project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chats.settings")  # NOQA
+django_asgi_app = get_asgi_application()  # NOQA
+
+from chats.apps.accounts.authentication.channels.middleware import (  # NOQA
+    TokenAuthMiddleware,
+)
+from chats.apps.api.websockets.rooms.routing import websocket_urlpatterns  # NOQA
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": TokenAuthMiddleware(URLRouter(websocket_urlpatterns)),
+    }
+)
