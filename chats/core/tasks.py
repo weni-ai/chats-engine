fc93893a50f5a91327ffdd900bf0466f4864721e
AlanJaeger <aland295@gmail.com>
2025-08-25 15:53:10 -0300

    fix: removing test tasks

--- chats/core/tasks.py
+++
@@ -1,9 +0,0 @@
-from chats.celery import app
-import logging
-
-logger = logging.getLogger(__name__)
-
-@app.task
-def beat_heartbeat():
-    logger.info("celery beat heartbeat OK")
-    return "ok"
