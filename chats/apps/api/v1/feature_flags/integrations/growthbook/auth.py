bcdb71e51508030a361489ecbd19404214b7b188
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2025-08-26 12:00:30 -0300

    feat: implement Growthbook webhook secret authentication
    - Introduced `GrowthbookWebhookSecretAuthentication` class for authenticating Growthbook webhooks using a secret from request headers.
    - Updated `GrowthbookWebhook` to utilize the new authentication class.
    - Modified tests to reflect changes in authentication logic, ensuring proper mocking of the new authentication method.

--- chats/apps/api/v1/feature_flags/integrations/growthbook/auth.py
+++ chats/apps/api/v1/feature_flags/integrations/growthbook/auth.py
@@ -42,3 +42,28 @@ class GrowthbookSignatureAuthentication(BaseAuthentication):
         should return 403 Forbidden responses.
         """
         return "Growthbook-Signature"
+
+
+class GrowthbookWebhookSecretAuthentication(BaseAuthentication):
+    """
+    Authentication for the Growthbook webhook using the secret provided in the request headers.
+    """
+
+    def authenticate(self, request):
+        """
+        Authenticate the Growthbook webhook using the secret provided in the request headers.
+        """
+        secret = request.headers.get("Secret")
+
+        if secret != settings.GROWTHBOOK_WEBHOOK_SECRET:
+            raise AuthenticationFailed("Invalid secret")
+
+        return (None, None)
+
+    def authenticate_header(self, request):
+        """
+        Return a string to be used as the WWW-Authenticate header in a
+        401 Unauthorized response, or None if the authentication scheme
+        should return 403 Forbidden responses.
+        """
+        return "Growthbook-Secret"
