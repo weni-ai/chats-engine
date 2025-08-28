3705d569910358b755e305f6826f3b260a25abb2
AlanJaeger <aland295@gmail.com>
2025-08-28 14:21:36 -0300

    feature: resolving flake8 errors

--- chats/apps/projects/usecases/send_room_info.py
+++ chats/apps/projects/usecases/send_room_info.py
@@ -1,5 +1,4 @@
 from typing import TYPE_CHECKING
-from django.conf import settings
 from chats.apps.api.v1.internal.eda_clients.billing_client import RoomsInfoMixin
 
 
@@ -13,7 +12,7 @@ class RoomInfoUseCase:
 
     def get_room(self, room: "Room"):
         project_uuid = self._get_project_uuid(room)
-        
+
         room_data = {
             "uuid": str(room.uuid),
             "project_uuid": project_uuid,
@@ -26,18 +25,18 @@ class RoomInfoUseCase:
         """Return the UUID of the secondary project if it's Infracommerce, otherwise the principal."""
         if not self._is_infracommerce_with_secondary(room):
             return str(room.project.uuid)
-        
+
         return room.queue.sector.config.get("secondary_project")
 
     def _is_infracommerce_with_secondary(self, room: "Room") -> bool:
-        """Check if it's an Infracommerce project with a secondary project configured."""        
+        """Check if it's an Infracommerce project with a secondary project configured."""
         if not room.project.config:
             return False
-        
+
         if not room.project.config.get("its_principal", False):
             return False
-        
+
         if not (room.queue and room.queue.sector and room.queue.sector.config):
             return False
-        
+
         return bool(room.queue.sector.config.get("secondary_project"))
