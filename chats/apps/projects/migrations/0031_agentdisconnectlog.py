c2f7d4cbb0c0f79281bef2734ae349a5c0d9c71e
AlanJaeger <aland295@gmail.com>
2025-09-02 15:43:28 -0300

    feature: resolving migration error

--- chats/apps/projects/migrations/0031_agentdisconnectlog.py
+++ chats/apps/projects/migrations/0031_agentdisconnectlog.py
@@ -10,7 +10,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ("projects", "0029_change_projects_room_routing_type"),
+        ("projects", "0030_project_internal_flags"),
     ]
 
     operations = [
