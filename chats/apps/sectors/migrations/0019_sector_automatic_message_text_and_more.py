0802c35e1d01921a2bc213a38e73da06712c6e66
AlanJaeger <aland295@gmail.com>
2025-09-09 15:31:32 -0300

    fix: migrations

--- chats/apps/sectors/migrations/0019_sector_automatic_message_text_and_more.py
+++ chats/apps/sectors/migrations/0019_sector_automatic_message_text_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('sectors', '0017_remove_sector_wordend_greater_than_workstart_check'),
+        ('sectors', '0018_remove_sectorholiday_unique_sector_holiday_and_more'),
     ]
 
     operations = [
