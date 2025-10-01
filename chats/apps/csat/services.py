63ba4e28e744ceb4bb648d387255620dbe9a39c5
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2025-10-01 15:32:52 -0300

    feat: enhance CSATFlowService with flow UUID caching and validation; add tests for flow retrieval and CSAT flow initiation

--- chats/apps/csat/services.py
+++ chats/apps/csat/services.py
@@ -2,34 +2,46 @@ from abc import ABC, abstractmethod
 from uuid import UUID
 
 from chats.apps.rooms.models import Room
+from django.core.exceptions import ValidationError
 from chats.apps.api.v1.internal.rest_clients.flows_rest_client import FlowRESTClient
-from chats.apps.csat.models import CSATFlowProjectConfig
+from chats.apps.csat.models import (
+    CSAT_FLOW_CACHE_KEY,
+    CSAT_FLOW_CACHE_TTL,
+    CSATFlowProjectConfig,
+)
 from chats.core.cache import BaseCacheClient
-
-
-CSAT_FLOW_CACHE_KEY = "csat_flow_uuid:{project_uuid}"
-CSAT_FLOW_CACHE_TTL = 300  # 5 minutes
+from chats.apps.api.authentication.token import JWTTokenGenerator
 
 
 class BaseCSATService(ABC):
+    @abstractmethod
+    def get_flow_uuid(self, project_uuid: UUID) -> UUID:
+        raise NotImplementedError
+
     @abstractmethod
     def start_csat_flow(self, room: Room) -> None:
         raise NotImplementedError
 
 
 class CSATFlowService(BaseCSATService):
-    def __init__(self, flows_client: FlowRESTClient, cache_client: BaseCacheClient):
+    def __init__(
+        self,
+        flows_client: FlowRESTClient,
+        cache_client: BaseCacheClient,
+        token_generator: JWTTokenGenerator,
+    ):
         self.flows_client = flows_client
         self.cache_client = cache_client
+        self.token_generator = token_generator
 
     def get_flow_uuid(self, project_uuid: UUID) -> UUID:
-        cache_key = CSAT_FLOW_CACHE_KEY.format(project_uuid=project_uuid)
+        cache_key = CSAT_FLOW_CACHE_KEY.format(project_uuid=str(project_uuid))
 
         if cached_flow_uuid := self.cache_client.get(cache_key):
             return UUID(cached_flow_uuid)
 
         flow_uuid = (
-            CSATFlowProjectConfig.objects.filter(project_uuid=project_uuid)
+            CSATFlowProjectConfig.objects.filter(project__uuid=project_uuid)
             .values_list("flow_uuid", flat=True)
             .first()
         )
@@ -40,3 +52,20 @@ class CSATFlowService(BaseCSATService):
         self.cache_client.set(cache_key, str(flow_uuid), CSAT_FLOW_CACHE_TTL)
 
         return flow_uuid
+
+    def start_csat_flow(self, room: Room) -> None:
+        if room.is_active:
+            raise ValidationError("Room is active")
+
+        flow_uuid = self.get_flow_uuid(room.project.uuid)
+        token = self.token_generator.generate_token(
+            {"project": str(room.project.uuid), "room": str(room.uuid)}
+        )
+
+        data = {
+            "flow": flow_uuid,
+            "urns": [room.urn],
+            "params": {"room": str(room.uuid), "token": token},
+        }
+
+        return self.flows_client.start_flow(room.project, data)
