8602d774b8be9d9ba618d62c4e75b703dbf73ce9
AlanJaeger <aland295@gmail.com>
2025-10-13 16:35:11 -0300

    fix: unit test that validate the old OIDC authentication

--- chats/apps/accounts/authentication/drf/backends.py
+++ chats/apps/accounts/authentication/drf/backends.py
@@ -1,6 +1,5 @@
 import json
 import logging
-import re
 
 from django.conf import settings
 from django.contrib.auth.models import Permission
@@ -37,7 +36,9 @@ class WeniOIDCAuthenticationBackend(OIDCAuthenticationBackend):
         """
         Cache OIDC token userinfo to avoid external calls to Keycloak
         """
+        LOGGER.info("Getting user info for access token")
         if not self.cache_token:
+            LOGGER.info("Not using cache for the get user info operation")
             return super().get_userinfo(access_token, *args)
 
         redis_connection = get_redis_connection()
@@ -45,17 +46,34 @@ class WeniOIDCAuthenticationBackend(OIDCAuthenticationBackend):
         userinfo = redis_connection.get(access_token)
 
         if userinfo is not None:
+            email = json.loads(userinfo).get("email")
+            LOGGER.info("Cached user info found for user %s", email)
             return json.loads(userinfo)
 
+        LOGGER.info("Cached info not found, getting user info from keycloak")
         userinfo = super().get_userinfo(access_token, *args)
 
+        LOGGER.info(
+            "User %s info fetched from keycloak, checking if it has can_communicate_internally",
+            userinfo.get("email"),
+        )
         can_communicate_internally = userinfo.get("can_communicate_internally", False)
+
+        LOGGER.info(
+            "User %s has can_communicate_internally: %s",
+            userinfo.get("email"),
+            can_communicate_internally,
+        )
 
         # Internal clients tokens have a longer cache time
         cache_ttl = (
             self.internal_token_cache_ttl
             if can_communicate_internally
             else self.cache_ttl
+        )
+
+        LOGGER.info(
+            "Setting cache for user %s with ttl %s", userinfo.get("email"), cache_ttl
         )
 
         redis_connection.set(access_token, json.dumps(userinfo), cache_ttl)
@@ -78,36 +96,38 @@ class WeniOIDCAuthenticationBackend(OIDCAuthenticationBackend):
         This eliminates the expensive UPPER(email) database query on every request.
         """
         user_info = self.get_userinfo(access_token, id_token, payload)
-        claims = self.get_claims(access_token, id_token, user_info)
-        
-        email = claims.get('email')
+
+        email = user_info.get("email")
         if not email:
             return None
-        
+
         user = get_cached_user(email)
-        
+
         if user:
-            user.first_name = claims.get("given_name", "")
-            user.last_name = claims.get("family_name", "")
-            user.save()
-            
-            invalidate_cached_user(email)
-            check_module_permission(claims, user)
-            
+            first_name = user_info.get("given_name", "")
+            last_name = user_info.get("family_name", "")
+
+            if user.first_name != first_name or user.last_name != last_name:
+                user.first_name = first_name
+                user.last_name = last_name
+                user.save()
+                invalidate_cached_user(email)
+
+            check_module_permission(user_info, user)
             return user
-        
+
         user, created = self.UserModel.objects.get_or_create(email=email)
-        
-        user.first_name = claims.get("given_name", "")
-        user.last_name = claims.get("family_name", "")
+
+        user.first_name = user_info.get("given_name", "")
+        user.last_name = user_info.get("family_name", "")
         user.save()
-        
+
         invalidate_cached_user(email)
-        check_module_permission(claims, user)
-        
+        check_module_permission(user_info, user)
+
         if created:
             LOGGER.info(f"Created new user: {email}")
-        
+
         return user
 
     def create_user(self, claims):
@@ -116,17 +136,17 @@ class WeniOIDCAuthenticationBackend(OIDCAuthenticationBackend):
         This method was causing redundant queries, now handled by get_or_create_user with cache.
         """
         email = claims.get("email")
-        
+
         user = get_cached_user(email)
-        
+
         if not user:
             user = self.UserModel.objects.create(email=email)
-        
+
         user.first_name = claims.get("given_name", "")
         user.last_name = claims.get("family_name", "")
         user.save()
-        
+
         invalidate_cached_user(email)
         check_module_permission(claims, user)
-        
+
         return user
