42adc9d18a8f9e39644f9e96b8c7d5085782cff4
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2025-10-14 11:27:55 -0300

    fix: ensure URN comparison is done with string type in RoomFlowViewSet

--- chats/apps/api/v1/external/rooms/viewsets.py
+++ chats/apps/api/v1/external/rooms/viewsets.py
@@ -254,7 +254,7 @@ class RoomFlowViewSet(viewsets.ModelViewSet):
 
         # [STAGING] Just a test
         urn_with_delay = CacheClient().get("urn_with_delay")
-        if urn_with_delay and instance.urn == urn_with_delay:
+        if urn_with_delay and instance.urn == str(urn_with_delay):
             print(f"{urn_with_delay} found in cache, delaying for 5 seconds")
             time.sleep(5)
 
