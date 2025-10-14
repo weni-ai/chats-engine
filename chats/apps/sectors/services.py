e561f94906c58dd9c4b71a56c79cac2b7a34f2c1
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2025-10-14 15:25:07 -0300

    feat: enhance ticket retrieval logic in AutomaticMessagesService with detailed logging and error handling

--- chats/apps/sectors/services.py
+++ chats/apps/sectors/services.py
@@ -37,14 +37,37 @@ class AutomaticMessagesService:
         ticket_uuid = room.ticket_uuid
 
         if ticket_uuid and check_ticket:
+            logger.info("[AUTOMATIC MESSAGES SERVICE] Checking ticket %s", ticket_uuid)
             wait_time = 1
 
             for i in range(FLOWS_GET_TICKET_RETRIES):
+                logger.info(
+                    "[AUTOMATIC MESSAGES SERVICE] Checking ticket %s (attempt %s)",
+                    ticket_uuid,
+                    i + 1,
+                )
                 response = FlowRESTClient().get_ticket(
                     room.queue.sector.project, ticket_uuid
                 )
+
                 if response.status_code == 200:
-                    break
+                    try:
+                        results = response.json().get("results")
+                    except Exception as e:
+                        capture_exception(e)
+                    else:
+                        if len(results) > 0:
+                            logger.info(
+                                "[AUTOMATIC MESSAGES SERVICE] Ticket %s found",
+                                ticket_uuid,
+                            )
+                            break
+
+                logger.info(
+                    "[AUTOMATIC MESSAGES SERVICE] Ticket %s not found. Retrying in %s seconds",
+                    ticket_uuid,
+                    wait_time,
+                )
 
                 time.sleep(wait_time)
                 wait_time *= 2
