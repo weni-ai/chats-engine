004b0324b31a50bbe81e7be37d41968158782c62
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2025-10-27 17:58:59 -0300

    Adding filters mapping to queries

--- chats/apps/api/v1/internal/dashboard/dto.py
+++ chats/apps/api/v1/internal/dashboard/dto.py
@@ -10,7 +10,9 @@ class Filters:
     agent: str = None
     sector: List = None
     queue: str = None
+    queues: List = None
     tag: str = None
+    tags: List = None
     is_weni_admin: bool = None
     user_request: ProjectPermission = None
     project: Project = None
