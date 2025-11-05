8d9f26557dc97499c8f169be30331b95689c5c44
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2025-11-05 18:43:31 -0300

    feat: enhance RoomsContactsInternalViewSet with ordering capabilities

--- chats/apps/api/v1/internal/contacts/views.py
+++ chats/apps/api/v1/internal/contacts/views.py
@@ -2,9 +2,8 @@ from rest_framework import status
 from rest_framework.mixins import ListModelMixin
 from rest_framework.viewsets import GenericViewSet
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
 from chats.apps.api.v1.internal.contacts.filters import RoomsContactsInternalFilter
 from chats.apps.api.v1.internal.contacts.serializers import (
     RoomsContactsInternalSerializer,
@@ -16,7 +15,9 @@ from chats.apps.contacts.models import Contact
 class RoomsContactsInternalViewSet(ListModelMixin, GenericViewSet):
     queryset = Contact.objects.all()
     serializer_class = RoomsContactsInternalSerializer
-    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = RoomsContactsInternalFilter
     permission_classes = [IsAuthenticated, ModuleHasPermission]
     search_fields = ["name"]
+    ordering = ["-created_on"]
+    ordering_fields = ["name", "created_on"]
