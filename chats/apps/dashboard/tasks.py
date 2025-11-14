a6d30320ce5c13e94a7a9544cb31c6b68074d2ea
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2025-11-14 17:18:48 -0300

    fix: adjust interaction time calculation to return total seconds in generate_metrics function

--- chats/apps/dashboard/tasks.py
+++ chats/apps/dashboard/tasks.py
@@ -78,7 +78,9 @@ def generate_metrics(room_uuid: UUID):
     metric_room.message_response_time = calculate_response_time(room)
 
     if not room.is_active and room.first_user_assigned_at:
-        metric_room.interaction_time = room.ended_at - room.first_user_assigned_at
+        metric_room.interaction_time = (
+            room.ended_at - room.first_user_assigned_at
+        ).total_seconds()
 
     if not room.user:
         metric_room.waiting_time += calculate_last_queue_waiting_time(room)
