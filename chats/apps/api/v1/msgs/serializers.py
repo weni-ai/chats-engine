93023489ab31c4914980a3e7e9c4883f8ca9cd5d
alandavl <aland295@gmail.com>
2025-12-03 17:56:57 -0300

    feature: Removing logs that were being sent to Sentry incorrectly (#761)
    * feature: Removing logs that were being sent to Sentry incorrectly

--- chats/apps/api/v1/msgs/serializers.py
+++ chats/apps/api/v1/msgs/serializers.py
@@ -269,6 +269,9 @@ class MessageSerializer(BaseMessageSerializer):
                 }
 
             return result
+        except ChatMessageReplyIndex.DoesNotExist:
+            # Replied message not found in index - expected scenario for projects that dont use whatsapp.
+            return None
         except Exception as error:
             LOGGER.error("Error getting replied message: %s", error)
             return None
