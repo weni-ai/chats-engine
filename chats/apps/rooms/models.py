dc5e5abda73fcda2505190ee089a08f103bb54b0
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2025-12-09 17:49:37 -0300

    refactor: simplify cache handling in Room model
    - Removed unnecessary context management for Redis connection in caching methods.
    - Streamlined the cache set and get operations for better readability and performance.

--- chats/apps/rooms/models.py
+++ chats/apps/rooms/models.py
@@ -317,17 +317,17 @@ class Room(BaseModel, BaseConfigurableModel):
         if ROOM_24H_VALID_CACHE_TTL == 0:
             return
 
-        with get_redis_connection() as redis_connection:
-            cache.set(
-                self.room_24h_valid_cache_key, is_24h_valid, ex=ROOM_24H_VALID_CACHE_TTL
-            )
+        cache.set(
+            self.room_24h_valid_cache_key,
+            is_24h_valid,
+            ex=ROOM_24H_VALID_CACHE_TTL,
+        )
 
     def get_24h_valid_from_cache(self) -> bool:
         if ROOM_24H_VALID_CACHE_TTL == 0:
             return None
 
-        with get_redis_connection() as redis_connection:
-            return cache.get(self.room_24h_valid_cache_key)
+        return cache.get(self.room_24h_valid_cache_key)
 
     def clear_24h_valid_cache(self) -> None:
         cache.delete(self.room_24h_valid_cache_key)
