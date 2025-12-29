ed245bcf814bd5b39aabe97f07cb3b40148ae671
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2025-12-29 10:56:23 -0300

    Add Archive Chats app with models and configurations
    - Introduced the Archive Chats app to manage archived conversations.
    - Added models for ArchiveConversationsJob and RoomArchivedConversation with relevant fields and methods.
    - Implemented choices for job status in ArchiveConversationsJobStatus.
    - Created upload function for archived conversation files.
    - Updated settings to include the new Archive Chats app.

---
+++ chats/apps/archive_chats/models.py
@@ -0,0 +1,111 @@
+import traceback
+from typing import Optional
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
+
+from chats.apps.archive_chats.choices import ArchiveConversationsJobStatus
+from chats.apps.archive_chats.uploads import upload_to
+from chats.apps.rooms.models import Room
+
+
+class ArchiveConversationsJob(models.Model):
+    started_at = models.DateTimeField(
+        _("Started at"),
+        null=True,
+        blank=True,
+        help_text=_("The date and time when the job started"),
+    )
+
+    class Meta:
+        verbose_name = _("Archive Conversations Job")
+        verbose_name_plural = _("Archive Conversations Jobs")
+
+    def __str__(self):
+        return f"Archive Conversations Job {self.id} - {self.started_at}"
+
+
+class RoomArchivedConversation(models.Model):
+    status = models.CharField(
+        verbose_name=_("Status"),
+        max_length=25,
+        choices=ArchiveConversationsJobStatus.choices,
+        default=ArchiveConversationsJobStatus.PENDING,
+    )
+    job = models.ForeignKey(
+        ArchiveConversationsJob,
+        verbose_name=_("Job"),
+        on_delete=models.CASCADE,
+        related_name="room_archived_conversations",
+        help_text=_("The job that archived the conversation"),
+    )
+    room = models.OneToOneField(
+        Room,
+        verbose_name=_("Room"),
+        on_delete=models.CASCADE,
+        related_name="archived_conversations",
+        help_text=_("The room that was archived"),
+    )
+    file = models.FileField(
+        verbose_name=_("File"),
+        upload_to=upload_to,
+        help_text=_("The file that contains the archived conversation"),
+    )
+    archive_process_started_at = models.DateTimeField(
+        verbose_name=_("Archive process started at"),
+        null=True,
+        blank=True,
+        help_text=_("The date and time when the archive process started"),
+    )
+    archive_process_finished_at = models.DateTimeField(
+        verbose_name=_("Archive process finished at"),
+        null=True,
+        blank=True,
+        help_text=_("The date and time when the conversation was archived"),
+    )
+    messages_deleted_at = models.DateTimeField(
+        verbose_name=_("Messages deleted at"),
+        null=True,
+        blank=True,
+        help_text=_(
+            "The date and time when the messages were deleted from the database"
+        ),
+    )
+    archive_process_finished_at = models.DateTimeField(
+        verbose_name=_("Archive process finished at"),
+        null=True,
+        blank=True,
+        help_text=_("The date and time when the archive process was finished"),
+    )
+    failed_at = models.DateTimeField(
+        verbose_name=_("Failed at"),
+        null=True,
+        blank=True,
+        help_text=_("The date and time when the archive process failed"),
+    )
+    errors = models.JSONField(
+        verbose_name=_("Errors"),
+        null=True,
+        blank=True,
+        help_text=_("The errors that occurred during the archive process"),
+    )
+
+    class Meta:
+        verbose_name = _("Room Archived Conversation")
+        verbose_name_plural = _("Room Archived Conversations")
+
+    def __str__(self):
+        return f"Room Archived Conversation {self.room.id} - {self.room.name}"
+
+    def register_error(self, error: Exception, sentry_event_id: Optional[str] = None):
+        self.errors.append(
+            {
+                "timestamp": timezone.now(),
+                "error": str(error),
+                "traceback": traceback.format_exception(
+                    type(error), error, error.__traceback__
+                ),
+                "sentry_event_id": sentry_event_id,
+            }
+        )
+        self.save(update_fields=["errors"])
