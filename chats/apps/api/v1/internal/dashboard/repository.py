e3230bfcbb82e7f5e7b5fea8386b4b1b66e4b6aa
Marcus Vinícius <marcusvinici25@gmail.com>
2026-01-28 16:01:30 -0300

    refactor: remove redundant date parsing for custom status filtering in AgentRepository

--- chats/apps/api/v1/internal/dashboard/repository.py
+++ chats/apps/api/v1/internal/dashboard/repository.py
@@ -130,22 +130,10 @@ class AgentRepository:
         if filters.agent:
             agents_query = agents_query.filter(email=filters.agent)
 
-        custom_status_start_date = (
-            pendulum_parse(filters.start_date, tzinfo=tz)
-            if filters.start_date
-            else initial_datetime
-        )
-        custom_status_end_date = (
-            pendulum_parse(filters.end_date + " 23:59:59", tzinfo=tz)
-            if filters.end_date
-            else timezone.now()
-        )
-
         custom_status_subquery = Subquery(
             CustomStatus.objects.filter(
                 user=OuterRef("email"),
                 status_type__project=project,
-                created_on__range=[custom_status_start_date, custom_status_end_date],
             )
             .values("user")
             .annotate(
