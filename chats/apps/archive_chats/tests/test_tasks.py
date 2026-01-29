476bbdf04a6ba568f7fc2f047332ee4b786f094d
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2026-01-29 16:16:20 -0300

    Add feature flag support for archiving projects
    - Introduced new settings to manage feature flags for archiving projects, allowing for more granular control over which projects are included in the archiving process.
    - Updated the `start_archive_rooms_messages` function to filter rooms based on the active projects list retrieved from the feature flags.
    - Enhanced the `ArchiveChatsService` with a method to fetch the projects list based on the feature flag, improving the flexibility of the archiving functionality.
    - Added unit tests to validate the behavior of the archiving process when the feature flag is active or inactive, ensuring robust handling of different scenarios.

--- chats/apps/archive_chats/tests/test_tasks.py
+++ chats/apps/archive_chats/tests/test_tasks.py
@@ -16,6 +16,9 @@ from chats.apps.archive_chats.tasks import (
     start_archive_rooms_messages,
 )
 from chats.apps.rooms.models import Room
+from chats.apps.projects.models import Project
+from chats.apps.sectors.models import Sector
+from chats.apps.queues.models import Queue
 
 service = MagicMock(spec=ArchiveChatsService)
 
@@ -25,6 +28,7 @@ class TestStartArchiveRoomsMessages(TestCase):
         service.reset_mock()
 
     @override_settings(ARCHIVE_CHATS_MAX_ROOMS=50)
+    @override_settings(ARCHIVE_CHATS_IS_ACTIVE_FOR_ALL_PROJECTS=True)
     @patch("chats.apps.archive_chats.tasks.ArchiveChatsService", return_value=service)
     @patch("chats.apps.archive_chats.tasks.archive_room_messages.apply_async")
     @patch("chats.apps.archive_chats.tasks.calculate_archive_task_expiration_dt")
@@ -84,7 +88,128 @@ class TestStartArchiveRoomsMessages(TestCase):
 
         assert mock_archive_room_messages_apply_async.call_count == len(rooms)
 
+    @override_settings(ARCHIVE_CHATS_MAX_ROOMS=50)
+    @override_settings(ARCHIVE_CHATS_IS_ACTIVE_FOR_ALL_PROJECTS=False)
+    @patch("chats.apps.archive_chats.tasks.ArchiveChatsService", return_value=service)
+    @patch("chats.apps.archive_chats.tasks.archive_room_messages.apply_async")
+    @patch("chats.apps.archive_chats.tasks.calculate_archive_task_expiration_dt")
+    def test_start_archive_rooms_messages_when_not_active_for_all_projects_and_projects_list_is_empty(
+        self,
+        mock_calculate_archive_task_expiration_dt,
+        mock_archive_room_messages_apply_async,
+        mock_archive_chats_service,
+    ):
+        mock_archive_room_messages_apply_async.return_value = None
+
+        expiration_dt = timezone.now() + timedelta(hours=1)
+        mock_calculate_archive_task_expiration_dt.return_value = expiration_dt
+
+        job = ArchiveConversationsJob.objects.create(
+            started_at=timezone.now(),
+        )
+        service.start_archive_job.return_value = job
+
+        # Should be included
+        rooms = [
+            Room.objects.create(
+                is_active=False,
+                ended_at=timezone.now() - rdelta(years=1),
+            )
+            for _ in range(2)
+        ]
+
+        # Should not be included because is active
+        Room.objects.create(is_active=True)
+        # Should not be included because ended_at is not more than 1 year ago
+        Room.objects.create(is_active=False, ended_at=timezone.now() - rdelta(days=3))
+        # Should not be included because room was already archived
+        room = Room.objects.create(
+            is_active=False, ended_at=timezone.now() - rdelta(years=1)
+        )
+        RoomArchivedConversation.objects.create(
+            room=room, job=job, status=ArchiveConversationsJobStatus.FINISHED
+        )
+
+        start_archive_rooms_messages()
+
+        service.start_archive_job.assert_called_once()
+        mock_archive_room_messages_apply_async.assert_not_called()
+
+    @override_settings(ARCHIVE_CHATS_MAX_ROOMS=50)
+    @override_settings(ARCHIVE_CHATS_IS_ACTIVE_FOR_ALL_PROJECTS=False)
+    @patch("chats.apps.archive_chats.tasks.ArchiveChatsService", return_value=service)
+    @patch("chats.apps.archive_chats.tasks.archive_room_messages.apply_async")
+    @patch("chats.apps.archive_chats.tasks.calculate_archive_task_expiration_dt")
+    def test_start_archive_rooms_messages_when_not_active_for_all_projects_and_projects_list_is_not_empty(
+        self,
+        mock_calculate_archive_task_expiration_dt,
+        mock_archive_room_messages_apply_async,
+        mock_archive_chats_service,
+    ):
+        mock_archive_room_messages_apply_async.return_value = None
+
+        expiration_dt = timezone.now() + timedelta(hours=1)
+        mock_calculate_archive_task_expiration_dt.return_value = expiration_dt
+
+        project = Project.objects.create(name="Test Project")
+        sector = Sector.objects.create(
+            name="Test Sector",
+            project=project,
+            rooms_limit=10,
+            work_start="09:00",
+            work_end="18:00",
+        )
+        queue = Queue.objects.create(name="Test Queue", sector=sector)
+
+        job = ArchiveConversationsJob.objects.create(
+            started_at=timezone.now(),
+        )
+        service.start_archive_job.return_value = job
+        service.get_projects.return_value = [project.uuid]
+
+        # Should be included
+        rooms = [
+            Room.objects.create(
+                is_active=False,
+                ended_at=timezone.now() - rdelta(years=1),
+                queue=queue,
+            )
+            for _ in range(2)
+        ]
+
+        # Should not be included because is active
+        Room.objects.create(is_active=True)
+        # Should not be included because ended_at is not more than 1 year ago
+        Room.objects.create(is_active=False, ended_at=timezone.now() - rdelta(days=3))
+        # Should not be included because room was already archived
+        room = Room.objects.create(
+            is_active=False, ended_at=timezone.now() - rdelta(years=1)
+        )
+        RoomArchivedConversation.objects.create(
+            room=room, job=job, status=ArchiveConversationsJobStatus.FINISHED
+        )
+
+        start_archive_rooms_messages()
+
+        service.start_archive_job.assert_called_once()
+
+        expected_calls = [
+            call(
+                args=[room.uuid, job.uuid],
+                expires=expiration_dt,
+            )
+            for room in rooms
+        ]
+
+        mock_archive_room_messages_apply_async.assert_has_calls(
+            expected_calls,
+            any_order=True,
+        )
+
+        assert mock_archive_room_messages_apply_async.call_count == len(rooms)
+
     @override_settings(ARCHIVE_CHATS_MAX_ROOMS=2)
+    @override_settings(ARCHIVE_CHATS_IS_ACTIVE_FOR_ALL_PROJECTS=True)
     @patch("chats.apps.archive_chats.tasks.ArchiveChatsService", return_value=service)
     @patch("chats.apps.archive_chats.tasks.archive_room_messages.apply_async")
     @patch("chats.apps.archive_chats.tasks.calculate_archive_task_expiration_dt")
