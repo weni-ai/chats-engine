d72fe0265abccd395a07a7b4037b21c283d7e7d7
AlanJaeger <aland295@gmail.com>
2026-02-03 16:42:32 -0300

    Merge feature/audio-description into staging

--- chats/apps/ai_features/audio_transcription/utils.py
+++ chats/apps/ai_features/audio_transcription/utils.py
@@ -1,13 +1,14 @@
 import io
 import logging
-import requests
+from typing import Optional
 
+import requests
 from pydub import AudioSegment
 
 logger = logging.getLogger(__name__)
 
 
-def get_audio_duration_seconds(media) -> float | None:
+def get_audio_duration_seconds(media) -> Optional[float]:
     """
     Get the duration of an audio file in seconds.
 
