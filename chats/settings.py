0ef7d8a57ea22c6e8f7d778fb5a8a7e1331afc1b
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2026-02-26 09:54:21 -0300

    feat: add feature flag for external API queryset filtering in RoomFlowViewSet

--- chats/settings.py
+++ chats/settings.py
@@ -688,3 +688,9 @@ INTERNAL_API_TOKEN = env.str("INTERNAL_API_TOKEN")
 VTEX_INTERNAL_DOMAINS = env.list(
     "VTEX_INTERNAL_DOMAINS", default=["weni.ai", "vtex.com"]
 )
+
+# Rooms External API QuerySet filter feature flag
+ROOMS_EXTERNAL_API_QUERYSET_FILTER_FLAG_KEY = env.str(
+    "ROOMS_EXTERNAL_API_QUERYSET_FILTER_FLAG_KEY",
+    default="weniChatsRoomsExternalApiQuerySetFilter",
+)
