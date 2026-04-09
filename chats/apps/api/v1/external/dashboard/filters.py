328492dd432407daa54b5d0a20362bc2fc84b6f0
AlanJaeger <aland295@gmail.com>
2026-04-09 16:40:05 -0300

    feat: correct user filter

--- chats/apps/api/v1/external/dashboard/filters.py
+++ chats/apps/api/v1/external/dashboard/filters.py
@@ -14,7 +14,7 @@ class ExternalFinishedRoomsStatusFilter(django_filters.FilterSet):
     sector = UUIDInFilter(field_name="queue__sector__uuid", lookup_expr="in")
     queue = UUIDInFilter(field_name="queue__uuid", lookup_expr="in")
     tag = UUIDInFilter(field_name="tags__uuid", lookup_expr="in")
-    agent = django_filters.CharFilter(field_name="user")
+    agent = django_filters.CharFilter(field_name="user__email")
 
     class Meta:
         model = Room
