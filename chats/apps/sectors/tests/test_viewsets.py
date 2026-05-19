ab64a86e21572e7a7ed2df457e9914232cb5b029
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2026-05-19 18:00:39 -0300

    fix(tests): Update feature flag patch in RoomsExternalTests to use correct viewset method

--- chats/apps/sectors/tests/test_viewsets.py
+++ chats/apps/sectors/tests/test_viewsets.py
@@ -256,7 +256,7 @@ class RoomsExternalTests(APITestCase):
     def setUp(self) -> None:
         self.queue_1 = Queue.objects.get(uuid="f2519480-7e58-4fc4-9894-9ab1769e29cf")
 
-    @patch("chats.apps.api.v1.external.rooms.serializers.is_feature_active", return_value=False)
+    @patch("chats.apps.api.v1.external.rooms.viewsets.is_feature_active_for_attributes", return_value=False)
     @patch("chats.apps.sectors.models.Sector.is_attending", return_value=True)
     @patch("chats.apps.projects.usecases.send_room_info.RoomInfoUseCase.get_room")
     def test_create_external_room(self, mock_get_room, mock_is_attending, _mock_flag):
@@ -279,7 +279,7 @@ class RoomsExternalTests(APITestCase):
         response = client.post(url, data=data, format="json")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-    @patch("chats.apps.api.v1.external.rooms.serializers.is_feature_active", return_value=False)
+    @patch("chats.apps.api.v1.external.rooms.viewsets.is_feature_active_for_attributes", return_value=False)
     @patch("chats.apps.sectors.models.Sector.is_attending", return_value=True)
     @patch("chats.apps.projects.usecases.send_room_info.RoomInfoUseCase.get_room")
     def test_create_external_room_with_external_uuid(
@@ -310,7 +310,7 @@ class RoomsExternalTests(APITestCase):
             "aec9f84e-3dcd-11ed-b878-0242ac190012",
         )
 
-    @patch("chats.apps.api.v1.external.rooms.serializers.is_feature_active", return_value=False)
+    @patch("chats.apps.api.v1.external.rooms.viewsets.is_feature_active_for_attributes", return_value=False)
     @patch("chats.apps.sectors.models.Sector.is_attending", return_value=True)
     @patch("chats.apps.projects.usecases.send_room_info.RoomInfoUseCase.get_room")
     def test_create_external_room_editing_contact(
