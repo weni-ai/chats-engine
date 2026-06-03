59f4dfa507506767dba4e9c713308b6e0570db5b
AlanJaeger <aland295@gmail.com>
2026-06-03 16:57:22 -0300

    feat: add AMQ_* settings for Amazon MQ consumer

--- chats/apps/event_driven/management/commands/edaconsume.py
+++ chats/apps/event_driven/management/commands/edaconsume.py
@@ -1,8 +1,30 @@
 from django.core.management.base import BaseCommand
+from django.utils.module_loading import import_string
 
 from ...base_app import EventDrivenAPP
 
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--params-class",
+            dest="params_class",
+            default=None,
+            help=(
+                "Dotted path to a ConnectionParamsFactory (e.g. "
+                "'weni.eda.django.AMQConnectionParamsFactory') to connect over "
+                "SSL on port 5671. If omitted, uses the default broker."
+            ),
+        )
+
     def handle(self, *args, **options):
+        params_class_path = options.get("params_class")
+
+        if params_class_path:
+            params_class = import_string(params_class_path)
+            from weni.eda.django.consumers import start_consuming
+
+            start_consuming(params_class)
+            return
+
         EventDrivenAPP().backend.start_consuming()
