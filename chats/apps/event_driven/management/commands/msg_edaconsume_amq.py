9968873b0e68e42f93e432de4c90727c5cd1ebaa
AlanJaeger <aland295@gmail.com>
2026-06-05 16:50:25 -0300

    fix commands

--- chats/apps/event_driven/management/commands/msg_edaconsume_amq.py
+++ chats/apps/event_driven/management/commands/msg_edaconsume_amq.py
@@ -3,6 +3,7 @@ import signal
 
 from django.core.management.base import BaseCommand
 from django.utils.module_loading import import_string
+from weni.eda.django.eda_app.management.commands.edaconsume import Command as WeniEDACommand
 
 
 def handle_sigterm(*args):
@@ -13,39 +14,6 @@ def handle_sigterm(*args):
     os._exit(0)
 
 
-class Command(BaseCommand):
-    """
-    Starts the Weni EDA consumer for message status events connected to the
-    Amazon MQ broker over SSL (port 5671) using
-    weni.eda.django.AMQConnectionParamsFactory.
-
-    Used by the docker-entrypoint.sh `edaconsumemsg-amq` alias.
-    """
-
-    AMQ_PARAMS_CLASS = "weni.eda.django.AMQConnectionParamsFactory"
-
-    def add_arguments(self, parser):
-        parser.add_argument(
-            "--group",
-            dest="group",
-            default="eda",
-            help=(
-                "Consumer group label, kept for parity with the entrypoint "
-                "aliases. Currently informational only."
-            ),
-        )
-
+class Command(WeniEDACommand):
     def handle(self, *args, **options):
-        signal.signal(signal.SIGTERM, handle_sigterm)
-
-        from django.conf import settings
-
-        # Point the EDA consumers handler to the messages handler so this AMQ
-        # process registers only message consumers on the Amazon MQ channel.
-        settings.EDA_CONSUMERS_HANDLE = "chats.apps.msgs.handle.handle_consumers"
-
-        params_class = import_string(self.AMQ_PARAMS_CLASS)
-
-        from weni.eda.django.consumers import start_consuming
-
-        start_consuming(params_class)
+        super().handle(*args, **options)
