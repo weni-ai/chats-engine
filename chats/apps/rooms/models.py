205be546d3903a0cb2e1e2e84095d79312a32795
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2026-06-23 11:36:22 -0300

    feat: allow null project association in RoomPin model

--- chats/apps/rooms/models.py
+++ chats/apps/rooms/models.py
@@ -919,6 +919,8 @@ class RoomPin(BaseModel):
         related_name="room_pins",
         verbose_name=_("project"),
         on_delete=models.CASCADE,
+        null=True,
+        blank=True,
     )
     created_on = models.DateTimeField(_("created on"), auto_now_add=True)
 
