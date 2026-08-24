4bf2341bedda9a07de5ea4afa38b8d5283dc507a
AlanJaeger <aland295@gmail.com>
2026-08-24 14:57:49 -0300

    feat: tag filter in external endpoint

--- chats/apps/api/v2/external/rooms/tests/test_views.py
+++ chats/apps/api/v2/external/rooms/tests/test_views.py
@@ -1,16 +1,15 @@
 from django.urls import reverse
-
 from rest_framework import status
-from rest_framework.test import APITestCase
 from rest_framework.response import Response
+from rest_framework.test import APITestCase
 
 from chats.apps.accounts.models import User
+from chats.apps.contacts.models import Contact
+from chats.apps.dashboard.models import RoomMetrics
 from chats.apps.projects.models.models import Project
 from chats.apps.queues.models import Queue
-from chats.apps.sectors.models import Sector
 from chats.apps.rooms.models import Room
-from chats.apps.dashboard.models import RoomMetrics
-from chats.apps.contacts.models import Contact
+from chats.apps.sectors.models import Sector, SectorTag
 
 
 class BaseTestExternalRoomMetrics(APITestCase):
@@ -57,3 +56,49 @@ class TestExternalRoomMetrics(BaseTestExternalRoomMetrics):
         response = self.list_rooms_metrics()
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_can_filter_rooms_metrics_by_tag_name_and_sector(self):
+        tag = SectorTag.objects.create(name="Atraso na Entrega", sector=self.sector)
+        self.room.tags.add(tag)
+
+        other_contact = Contact.objects.create(name="Other Contact")
+        other_room = Room.objects.create(
+            contact=other_contact,
+            queue=self.queue,
+            user=self.user,
+            project_uuid=str(self.project.uuid),
+        )
+        RoomMetrics.objects.create(room=other_room, waiting_time=5)
+
+        other_sector = Sector.objects.create(
+            name="Other Sector",
+            project=self.project,
+            rooms_limit=10,
+            work_start="09:00",
+            work_end="18:00",
+        )
+        other_queue = Queue.objects.create(name="Other Queue", sector=other_sector)
+        same_name_tag = SectorTag.objects.create(
+            name="Atraso na Entrega",
+            sector=other_sector,
+        )
+        other_sector_room = Room.objects.create(
+            contact=Contact.objects.create(name="Other Sector Contact"),
+            queue=other_queue,
+            user=self.user,
+            project_uuid=str(self.project.uuid),
+        )
+        other_sector_room.tags.add(same_name_tag)
+        RoomMetrics.objects.create(room=other_sector_room, waiting_time=5)
+
+        response = self.list_rooms_metrics(
+            {
+                "sector": str(self.sector.uuid),
+                "tag": "Atraso na Entrega",
+            }
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        results = response.data["results"]
+        self.assertEqual(len(results), 1)
+        self.assertEqual(str(results[0]["uuid"]), str(self.room.uuid))
