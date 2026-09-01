e64f8db41088eff70f634fa61acce63e3dc71d15
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2026-09-01 11:07:30 -0300

    fix(tests): remove unnecessary blank line in test case for message viewset

--- chats/apps/api/v1/msgs/tests/test_viewsets.py
+++ chats/apps/api/v1/msgs/tests/test_viewsets.py
@@ -431,4 +431,3 @@ class TestMessageViewsetCreateWithMedia(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn("media", response.data)
-
