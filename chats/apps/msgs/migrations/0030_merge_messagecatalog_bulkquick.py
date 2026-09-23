49eb7eeae6548a83c2db58866d019c8b0233533f
Marcus Vinícius <marcusvinici25@gmail.com>
2026-09-23 19:29:31 -0300

    chore: add migration to merge message catalog dependencies

---
+++ chats/apps/msgs/migrations/0030_merge_messagecatalog_bulkquick.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("msgs", "0029_bulkquickmessagesend_bulkquickmessagesendmessage"),
+        ("msgs", "0029_messagecatalog"),
+    ]
+
+    operations = []
