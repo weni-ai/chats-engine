556f892f3dd081ec68f72f8a47bf9fdcffacce14
Marcus Vinícius <marcusvinici25@gmail.com>
2026-09-23 19:17:20 -0300

    feat: add MessageCatalog model to manage message-related data

--- chats/apps/msgs/migrations/0029_messagecatalog.py
+++ chats/apps/msgs/migrations/0029_messagecatalog.py
@@ -6,7 +6,7 @@ import uuid
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("msgs", "0029_bulkquickmessagesend_bulkquickmessagesendmessage"),
+        ("msgs", "0028_messagemedia_room_alter_messagemedia_message"),
     ]
 
     operations = [
